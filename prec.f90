987c3b9d89577b0236f1907f9b42a93b22ecf475
Philip	Eisenlohr <eisenlohr@egr.msu.edu>
2007-03-20 13:50:57 +0000

    second initial import ;-)

---
+++ prec.f90
@@ -0,0 +1,11 @@
+
+!##############################################################
+ MODULE prec
+!##############################################################
+
+ implicit none
+
+ integer, parameter :: pReal = 8
+ integer, parameter :: pInz  = 4
+
+ END MODULE prec
