9da4b4cf2417fdc2ffa76205b5c1bdfdfd4a8e6b
Philip	Eisenlohr <eisenlohr@egr.msu.edu>
2007-04-11 10:05:08 +0000

    name change to reflect State and Stress (was outer,inner) added nReg for Jacobi regularization attempts

--- trunk/prec.f90
+++ trunk/prec.f90
@@ -11,19 +11,19 @@
 !    *** How frequently the jacobian is recalculated ***
  integer (pInt), parameter :: ijaco = 5_pInt
 !    *** Maximum number of internal cutbacks in time step ***
- integer(pInt), parameter :: ncut=7_pInt
+ integer(pInt), parameter :: nCutback = 7_pInt
 !    *** Maximum number of regularization attempts for Jacobi inversion ***
- integer(pInt), parameter :: nreg=1_pInt
+ integer(pInt), parameter :: nReg = 1_pInt
 !    *** Perturbation of strain array for numerical calculation of FEM Jacobi matrix ***
  real(pReal), parameter :: pert_e=1.0e-5_pReal  
-!    *** Maximum number of iterations in outer (statevariables) loop ***
- integer(pInt), parameter :: nouter    = 50_pInt
-!    *** Convergence criteria for outer (statevariables) loop ***
- real(pReal),   parameter :: tol_outer = 1.0e-4_pReal
+!    *** Maximum number of iterations in outer (state variables) loop ***
+ integer(pInt), parameter :: nState    = 50_pInt
+!    *** Convergence criteria for outer (state variables) loop ***
+ real(pReal),   parameter :: tol_State = 1.0e-4_pReal
 !    *** Maximum number of iterations in inner (stress) loop ***
- integer(pInt), parameter :: ninner    = 2000_pInt
+ integer(pInt), parameter :: nStress    = 2000_pInt
 !    *** Convergence criteria for inner (stress) loop ***
- real(pReal),   parameter :: tol_inner = 1.0e-3_pReal   
+ real(pReal),   parameter :: tol_Stress = 1.0e-3_pReal   
 !    *** Factor for maximum stress correction in inner (stress) loop ***
  real(pReal),   parameter :: crite     = 1.0e-1_pReal 
 
