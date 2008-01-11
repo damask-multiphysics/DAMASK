6a3a9e8f98d50cf8fea1480d414c2eb552135bfc
Hongmei	Li <li.h@mpie.de>
2008-01-11 18:32:02 +0000

    Added more detailed reporting

--- trunk/debug.f90
+++ trunk/debug.f90
@@ -5,7 +5,7 @@
  use prec
 
  implicit none
- integer(pInt), dimension(nCutback) :: debug_cutbackDistribution
+ integer(pInt), dimension(nCutback+1) :: debug_cutbackDistribution
  integer(pInt), dimension(nStress) :: debug_stressLoopDistribution
  integer(pInt), dimension(nState) :: debug_stateLoopDistribution
 
@@ -24,21 +24,24 @@
 
  write(6,*) 'DEBUG Info'
  write(6,*)	'distribution_cutback :'
- do i=1,nCutback
-   if (debug_cutbackDistribution(i) > 0) write(6,*) i,debug_cutbackDistribution(i)
+ do i=0,nCutback
+   if (debug_cutbackDistribution(i+1) > 0) write(6,*) i,debug_cutbackDistribution(i+1)
  enddo
+ write(6,*) 'total',sum(debug_cutbackDistribution)
  write(6,*)
  
  write(6,*)	'distribution_stressLoop :'
  do i=1,nStress
    if (debug_stressLoopDistribution(i) > 0) write(6,*) i,debug_stressLoopDistribution(i)
  enddo
+ write(6,*) 'total',sum(debug_stressLoopDistribution)
  write(6,*)
  
  write(6,*)	'distribution_stateLoop :'
  do i=1,nState
    if (debug_stateLoopDistribution(i) > 0) write(6,*) i,debug_stateLoopDistribution(i)
  enddo
+ write(6,*) 'total',sum(debug_stateLoopDistribution)
  write(6,*)
 
  END SUBROUTINE
