51cc6714d09f66b9e2b29ba5fbe8848a04e220ca
Franz	Roters <f.roters@mpie.de>
2009-03-31 09:07:30 +0000

    changed pLongInt to 8

--- trunk/prec.f90
+++ trunk/prec.f90
@@ -8,7 +8,7 @@
 !    *** Precision of real and integer variables ***
  integer, parameter :: pReal = selected_real_kind(15,300)  ! 15 significant digits, up to 1e+-300
  integer, parameter :: pInt  = selected_int_kind(9)        ! up to +- 1e9
- integer, parameter :: pLongInt  = 4                       ! should be 64bit
+ integer, parameter :: pLongInt  = 8                       ! should be 64bit
 
 
  type :: p_vec
