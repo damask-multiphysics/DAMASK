ab82dbc99ca79c160db6293b695a68c60018d7f9
Martin	Diehl <m.diehl@mpie.de>
2010-11-30 14:26:30 +0000

    continued working on reading header of output file

--- processing/post/spectral_post.py
+++ processing/post/spectral_post.py
@@ -1,18 +1,39 @@
+import binascii
 print('post processing for mpie_spectral')
 
 results = open('results.out', 'rb')
 
-header = results.read(4)
-end = results.read(1024).find(header)
-results.seek(4)
-loadcase = results.read(end)
+header = results.read(4) #read header
+begin = results.tell()
+
+end = results.read(1024).find(header) #find header (second time)
+
+results.seek(begin+9) #position: header + string "loadcase "
+#loadcase = binascii.b2a_base64(results.read(end-9)) #conversion doesnt work
+loadcase = results.read(end -9)
 print('loadcase:', loadcase)
-#
-begin = end
-results.seek(end+4)
+
+
+results.seek(begin+end+4)
+
 header = results.read(4)
-print(header)
-end = results.read(1024).find(header)
-#results.seek(4)
-#workingdir = results.read(end)
-#print('workingdir:', workingdir)
+begin = results.tell()
+end = results.read(1024).find(header) #find header (second time)
+results.seek(begin+11) #position: header + string "workingdir "
+workingdir = results.read(end -11)
+print('workingdir:', workingdir)
+#
+results.seek(begin+end+4)
+
+header = results.read(4)
+begin = results.tell()
+end = results.read(1024).find(header) #find header (second time)
+results.seek(begin+8) #position: header + string "workingdir "
+jobname = results.read(end -8)
+print('jobname:', jobname)
+
+header = b'a'
+results.seek(begin+end+19)
+print(results.seek(begin+end+19))
+begin = results.read(1024).find(header)
+print(begin)
