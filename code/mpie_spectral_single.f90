fcdb80522522dc63675dcf46da8e94209beaec01
Franz	Roters <f.roters@mpie.de>
2011-04-04 14:09:54 +0000

    added copyright text to all f90 (free) format files

--- code/mpie_spectral_single.f90
+++ code/mpie_spectral_single.f90
@@ -1,3 +1,22 @@
+! Copyright 2011 Max-Planck-Institut für Eisenforschung GmbH
+!
+! This file is part of DAMASK,
+! the Düsseldorf Advanced MAterial Simulation Kit.
+!
+! DAMASK is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! DAMASK is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with DAMASK. If not, see <http://www.gnu.org/licenses/>.
+!
+!##############################################################
 !* $Id: mpie_spectral.f90 769 2011-02-21 14:37:38Z MPIE\m.diehl $
 !********************************************************************
 ! Material subroutine for BVP solution using spectral method
