e7a60988a1889f245deb9621b60b09ecb31641c7
Claudio	Zambaldi <c.zambaldi@mpie.de>
2011-10-24 13:02:08 +0000

    modified exit number extraction

--- processing/post/msc_tools.py
+++ processing/post/msc_tools.py
@@ -5,8 +5,10 @@ class MSC_TOOLS():
      for ln in fid_out:
        if (string.find(ln,'tress iteration') is not -1):
          print ln
+         fid_out.close()
        elif (string.find(ln,'Exit number') is not -1):
          substr=ln[string.find(ln,'Exit number'):len(ln)]
          exitnumber=substr[12:16]
-     fid_out.close()
-     return exitnumber
+         fid_out.close()
+         return exitnumber
+     return -1    
