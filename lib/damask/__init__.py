a27a78bab1896a4749552ba947d593390df0af3b
Claudio	Zambaldi <c.zambaldi@mpie.de>
2011-12-15 15:15:32 +0000

    changed per Philips suggestion config folder will later contain debug.py and numerics.py

--- lib/damask/__init__.py
+++ lib/damask/__init__.py
@@ -1,6 +1,6 @@
 from .environment import Environment      # only one class
 from .asciitable  import ASCIItable       # only one class
-from .material    import Material
+from .config      import Material         # will be extended to debug and numerics
 from .result      import Result           # one class with subclasses
 from .geometry    import Geometry         # one class with subclasses
 from .solver      import Solver           # one class with subclasses
