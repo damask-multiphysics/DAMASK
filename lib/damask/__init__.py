ae8ec80dd09a3cc33bc99dc7bcc6ae6d32141569
Philip	Eisenlohr <eisenlohr@egr.msu.edu>
2012-02-16 18:02:15 +0000

    croaking about missing core module now goes to STDERR (formerly this influenced piped STDOUT detrimentally...)

--- lib/damask/__init__.py
+++ lib/damask/__init__.py
@@ -1,8 +1,10 @@
 # $Id$
+import sys
 
 from .environment import Environment      # only one class
 from .asciitable  import ASCIItable       # only one class
 from .config      import Material         # will be extended to debug and numerics
+#from .block       import Block            # only one class
 from .result      import Result           # one class with subclasses
 from .geometry    import Geometry         # one class with subclasses
 from .solver      import Solver           # one class with subclasses
@@ -10,4 +12,4 @@ from .test        import Test
 try:
   from .          import core
 except:
-  print 'Core module (Fortran code) not available, try to run setup_processing.py'
+  sys.stderr.write('Core module (Fortran code) not available, try to run setup_processing.py\n')
