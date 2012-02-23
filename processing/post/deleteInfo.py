53d7076d05625d0fd48ec7ca87d0a708fdc049ee
Philip	Eisenlohr <eisenlohr@egr.msu.edu>
2012-02-23 13:53:50 +0000

    remove the info part from ASCIItables

---
+++ processing/post/deleteInfo.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import os,re,sys,math,string,damask
+from optparse import OptionParser, Option
+
+# -----------------------------
+class extendableOption(Option):
+# -----------------------------
+# used for definition of new option parser action 'extend', which enables to take multiple option arguments
+# taken from online tutorial http://docs.python.org/library/optparse.html
+  
+  ACTIONS = Option.ACTIONS + ("extend",)
+  STORE_ACTIONS = Option.STORE_ACTIONS + ("extend",)
+  TYPED_ACTIONS = Option.TYPED_ACTIONS + ("extend",)
+  ALWAYS_TYPED_ACTIONS = Option.ALWAYS_TYPED_ACTIONS + ("extend",)
+
+  def take_action(self, action, dest, opt, value, values, parser):
+    if action == "extend":
+      lvalue = value.split(",")
+      values.ensure_value(dest, []).extend(lvalue)
+    else:
+      Option.take_action(self, action, dest, opt, value, values, parser)
+
+
+
+# --------------------------------------------------------------------
+#                                MAIN
+# --------------------------------------------------------------------
+
+parser = OptionParser(option_class=extendableOption, usage='%prog [file[s]]', description = """
+Remove info from table head
+""" + string.replace('$Id$','\n','\\n')
+)
+
+(options,filenames) = parser.parse_args()
+
+
+# ------------------------------------------ setup file handles ---------------------------------------  
+
+files = []
+if filenames == []:
+  files.append({'name':'STDIN', 'input':sys.stdin, 'output':sys.stdout})
+else:
+  for name in filenames:
+    if os.path.exists(name):
+      files.append({'name':name, 'input':open(name), 'output':open(name+'_tmp','w')})
+
+# ------------------------------------------ loop over input files ---------------------------------------  
+
+for file in files:
+  if file['name'] != 'STDIN': print file['name']
+
+  table = damask.ASCIItable(file['input'],file['output'],False)             # make unbuffered ASCII_table
+  table.head_read()                                                         # read ASCII header info
+  table.info_clear()
+
+# ------------------------------------------ assemble header ---------------------------------------  
+
+  table.head_write()
+
+# ------------------------------------------ process data ---------------------------------------  
+
+  outputAlive = True
+  while outputAlive and table.data_read():                                  # read next data line of ASCII table
+    outputAlive = table.data_write()                                        # output processed line
+
+# ------------------------------------------ output result ---------------------------------------  
+
+  outputAlive and table.output_flush()                                      # just in case of buffered ASCII table
+
+  file['input'].close()                                                     # close input ASCII table
+  if file['name'] != 'STDIN':
+    file['output'].close()                                                  # close output ASCII table
+    os.rename(file['name']+'_tmp',file['name'])                             # overwrite old one with tmp new
