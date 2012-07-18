b2a7f8510130a2ce3a52800cfe4405b1e6d42e9a
Claudio	Zambaldi <c.zambaldi@mpie.de>
2012-07-18 13:46:33 +0000

    __init__ of damask package not crashing in python 2.5 if core module not available added some Matlab-like utility functions (damask.sind, cosd, tand, acosd, ...)  that take and return degree instead of radians

--- lib/damask/__init__.py
+++ lib/damask/__init__.py
@@ -9,7 +9,12 @@ from .result      import Result           # one class with subclasses
 from .geometry    import Geometry         # one class with subclasses
 from .solver      import Solver           # one class with subclasses
 from .test        import Test
+from util import *
+
 try:
   from .          import core
-except Exception, e:
-  sys.stderr.write('\nWARNING: Core module (Fortran code) not available, try to run setup_processing.py\nError Message when importing core.so: %s\n\n'%e)
+except ImportError:
+  sys.stderr.write('\nWARNING: Core module (Fortran code) not available, try to run setup_processing.py\nError Message when importing core.so: \n\n')
+  core = None # from http://www.python.org/dev/peps/pep-0008/
+  
+  
