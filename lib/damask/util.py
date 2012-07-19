9078d70af0361cef9d7bb16b03ddc924a18ebe39
Claudio	Zambaldi <c.zambaldi@mpie.de>
2012-07-19 16:05:05 +0000

    polishing, and using numpy's deg2rad/rad2deg

--- lib/damask/util.py
+++ lib/damask/util.py
@@ -1,22 +1,16 @@
-# some utility functions
+# damask utility functions
 import math 
 try:
   import numpy
 except:
   numpy=None
 
-  
-
-
-
-
+# Matlab like trigonometric functions that take and return angles in degrees.
 for f in ['cos', 'sin', 'tan']:
-    exec('def %sd(deg): return ((numpy or math).%s(deg/180.*math.pi))'%(f,f))
     if numpy:
-      exec('def a%sd(val): return (numpy.arc%s(val)*180./math.pi)'%(f,f))
+      exec('def %sd(deg): return (numpy.%s(numpy.deg2rad(deg)))'%(f,f))
+      exec('def a%sd(val): return (numpy.rad2deg(numpy.arc%s(val)))'%(f,f))
     else:  
+      exec('def %sd(deg): return (math.%s(deg/180.*math.pi))'%(f,f))
       exec('def a%sd(val): return (math.a%s(val)*180./math.pi)'%(f,f))
-# Matlab like functions to allow for cosd(degree),...
-# it is open how these compare to the numpy.cos, which can operate on arrays
-# ->full array compatibility can be achieved through numpy.deg2rad|rad2deg
 
