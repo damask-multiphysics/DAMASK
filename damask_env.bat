9887f42383f47a5e14220a7e92e85a5fe6df89f6
Claudio	Zambaldi <c.zambaldi@mpie.de>
2012-07-20 15:14:08 +0000

    typo and polishing

--- damask_env.bat
+++ damask_env.bat
@@ -1,18 +1,15 @@
 @echo off
 chcp 1252
-Title Düsseldorf Adavanced MAterials Simulation Kit - DAMASK, MPIE Düsseldorf
+Title Düsseldorf Advanced MAterials Simulation Kit - DAMASK, MPIE Düsseldorf
 
 echo.
-echo Düsseldorf Adavanced MAterials Simulation Kit - DAMASK
-echo Max-Planck-Institut für Eisenforschung Düsseldorf
+echo Düsseldorf Advanced MAterials Simulation Kit - DAMASK
+echo Max-Planck-Institut für Eisenforschung, Düsseldorf
 echo http://damask.mpie.de
 echo.
-
-::echo %cd%
-
 echo Preparing environment ...
+
 SET DAMASK_ROOT=%~dp0
-::SET PYTHONPATH=%DAMASK_ROOT%\lib;%PYTHONPATH%
 SET PYTHONPATH=%DAMASK_ROOT%\lib;%PYTHONPATH%
 
 echo DAMASK_ROOT: %DAMASK_ROOT%
