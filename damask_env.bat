1a25f0e5b917d6636264378d384f07f74f8ab2c9
Claudio	Zambaldi <c.zambaldi@mpie.de>
2012-07-20 15:01:31 +0000

    Added a batch file & shortcut to prepare the DAMASK environment on Windows The batch file sets DAMASK_ROOT and PYTHONPATH.

---
+++ damask_env.bat
@@ -0,0 +1,23 @@
+@echo off
+chcp 1252
+Title Düsseldorf Adavanced MAterials Simulation Kit - DAMASK, MPIE Düsseldorf
+
+echo.
+echo Düsseldorf Adavanced MAterials Simulation Kit - DAMASK
+echo Max-Planck-Institut für Eisenforschung Düsseldorf
+echo http://damask.mpie.de
+echo.
+
+::echo %cd%
+
+echo Preparing environment ...
+SET DAMASK_ROOT=%~dp0
+::SET PYTHONPATH=%DAMASK_ROOT%\lib;%PYTHONPATH%
+SET PYTHONPATH=%DAMASK_ROOT%\lib;%PYTHONPATH%
+
+echo DAMASK_ROOT: %DAMASK_ROOT%
+
+cd %~dp0  
+
+::python -c "import damask as D" -i
+ipython -c "import damask as D; import os; os.chdir(os.getenv('DAMASK_ROOT')); print('pwd: '+os.getcwd());" -i
