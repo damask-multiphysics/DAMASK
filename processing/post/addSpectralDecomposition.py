255af58434b0dfc5743077d043b7b0908c32a5aa
Philip	Eisenlohr <eisenlohr@egr.msu.edu>
2012-11-23 21:49:30 +0000

    post processing script to add eigenvalues and eigenvectors to tensorial data in ASCII tables.

---
+++ processing/post/addSpectralDecomposition.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+import os,re,sys,math,numpy,string,damask
+from optparse import OptionParser, Option
+
+# -----------------------------
+class extendableOption(Option):
+# -----------------------------
+# used for definition of new option parser action 'extend', which enables to take multiple option arguments
+# taken from online tutorial http://docs.python.org/library/optparse.html
+  
+  ACTIONS = Option.ACTIONS + ("extend",)
+  STORE_ACTIONS = Option.STORE_ACTIONS + ("extend",)
+  TYPED_ACTIONS = Option.TYPED_ACTIONS + ("extend",)
+  ALWAYS_TYPED_ACTIONS = Option.ALWAYS_TYPED_ACTIONS + ("extend",)
+
+  def take_action(self, action, dest, opt, value, values, parser):
+    if action == "extend":
+      lvalue = value.split(",")
+      values.ensure_value(dest, []).extend(lvalue)
+    else:
+      Option.take_action(self, action, dest, opt, value, values, parser)
+
+
+# --------------------------------------------------------------------
+#                                MAIN
+# --------------------------------------------------------------------
+
+parser = OptionParser(option_class=extendableOption, usage='%prog options [file[s]]', description = """
+Add column(s) containing eigenvalues and eigenvectors of requested tensor column(s).
+
+""" + string.replace('$Id$','\n','\\n')
+)
+
+
+parser.add_option('-t','--tensor',      dest='tensor', action='extend', type='string', \
+                                        help='heading of columns containing tensor field values')
+
+parser.set_defaults(tensor = [])
+
+(options,filenames) = parser.parse_args()
+
+if len(options.tensor) == 0:
+  parser.error('no data column specified...')
+
+datainfo = {                                                               # list of requested labels per datatype
+             'tensor':     {'len':9,
+                            'label':[]},
+           }
+
+
+if options.tensor != None:    datainfo['tensor']['label'] += options.tensor
+
+# ------------------------------------------ setup file handles ---------------------------------------  
+
+files = []
+if filenames == []:
+  files.append({'name':'STDIN',
+                'input':sys.stdin,
+                'output':sys.stdout,
+                'croak':sys.stderr,
+               })
+else:
+  for name in filenames:
+    if os.path.exists(name):
+      files.append({'name':name,
+                    'input':open(name),
+                    'output':open(name+'_tmp','w'),
+                    'croak':sys.stdout,
+                    })
+
+# ------------------------------------------ loop over input files ---------------------------------------  
+
+for file in files:
+  if file['name'] != 'STDIN': file['croak'].write(file['name']+'\n')
+
+
+  table = damask.ASCIItable(file['input'],file['output'],False)             # make unbuffered ASCII_table
+  table.head_read()                                                         # read ASCII header info
+  table.info_append(string.replace('$Id$','\n','\\n') + \
+                    '\t' + ' '.join(sys.argv[1:]))
+
+  active = {}
+  column = {}
+  head = []
+
+  for datatype,info in datainfo.items():
+    for label in info['label']:
+      key = {True :'1_%s',
+             False:'%s'   }[info['len']>1]%label
+      if key not in table.labels:
+        file['croak'].write('column %s not found...\n'%key)
+      else:
+        if datatype not in active: active[datatype] = []
+        if datatype not in column: column[datatype] = {}
+        active[datatype].append(label)
+        column[datatype][label] = table.labels.index(key)                   # remember columns of requested data
+        table.labels_append(['%i_eigval(%s)'%(i+1,label) 
+                            for i in xrange(3)])                            # extend ASCII header with new labels
+        table.labels_append(['%i_eigvec(%s)'%(i+1,label) 
+                            for i in xrange(9)])                            # extend ASCII header with new labels
+
+# ------------------------------------------ assemble header ---------------------------------------  
+
+  table.head_write()
+
+# ------------------------------------------ process data ---------------------------------------  
+
+  while table.data_read():                                                  # read next data line of ASCII table
+  
+    for datatype,labels in active.items():                                  # loop over vector,tensor
+      for label in labels:                                                  # loop over all requested norms
+        tensor = numpy.array(map(float,table.data[column[datatype][label]:
+                                                  column[datatype][label]+datainfo[datatype]['len']])).reshape((datainfo[datatype]['len']//3,3))
+        (u,v) = numpy.linalg.eig(tensor)
+        table.data_append(list(u))
+        table.data_append(list(v.transpose().reshape(datainfo[datatype]['len'])))
+
+    table.data_write()                                                      # output processed line
+
+# ------------------------------------------ output result ---------------------------------------  
+
+  table.output_flush()                                                      # just in case of buffered ASCII table
+
+  try:
+    file['output'].close()                                                  # close output ASCII table
+  except:
+    pass
+  try:
+    file['croak'].close()                                                   # stop croaking
+  except:
+    pass
+  try:
+    file['input'].close()                                                   # close input ASCII table
+  except:
+    pass
+
+  if file['name'] != 'STDIN':
+    os.rename(file['name']+'_tmp',file['name'])                             # overwrite old one with tmp new
