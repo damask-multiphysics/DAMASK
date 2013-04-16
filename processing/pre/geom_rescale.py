d971a32516321cbd4d38505fb93af8cb37896d81
Martin	Diehl <m.diehl@mpie.de>
2013-04-16 18:00:14 +0000

    changed keywords, now also printing out altered size/grid/microstructures information

--- processing/pre/geom_rescale.py
+++ processing/pre/geom_rescale.py
@@ -27,32 +27,33 @@ class extendedOption(Option):
 # ----------------------- MAIN -------------------------------
 
 identifiers = {
-        'resolution': ['a','b','c'],
-        'dimension':  ['x','y','z'],
-        'origin':     ['x','y','z'],
+        'grid':   ['a','b','c'],
+        'size':   ['x','y','z'],
+        'origin': ['x','y','z'],
           }
 mappings = {
-        'resolution': lambda x: int(x),
-        'dimension':  lambda x: float(x),
-        'origin':     lambda x: float(x),
-        'homogenization': lambda x: int(x),
+        'grid':            lambda x: int(x),
+        'size':            lambda x: float(x),
+        'origin':          lambda x: float(x),
+        'homogenization':  lambda x: int(x),
+        'microstructures': lambda x: int(x),
           }
 
 
 parser = OptionParser(option_class=extendedOption, usage='%prog options [file[s]]', description = """
-Scales a geometry description independently in x, y, and z direction in terms of resolution and/or dimension.
+Scales a geometry description independently in x, y, and z direction in terms of grid and/or size.
 """ + string.replace('$Id$','\n','\\n')
 )
 
-parser.add_option('-r', '--resolution', dest='resolution', type='int', nargs = 3, \
-                  help='new resolution (a,b,c)')
-parser.add_option('-d', '--dimension', dest='dimension', type='float', nargs = 3, \
-                  help='new dimension (x,y,z)')
+parser.add_option('-g', '--grid', dest='grid', type='int', nargs = 3, \
+                  help='a,b,c grid of hexahedral box [unchanged]')
+parser.add_option('-s', '--size', dest='size', type='float', nargs = 3, \
+                  help='x,y,z size of hexahedral box [unchanged]')
 parser.add_option('-2', '--twodimensional', dest='twoD', action='store_true', \
                   help='output geom file with two-dimensional data arrangement')
 
-parser.set_defaults(resolution = [0,0,0])
-parser.set_defaults(dimension  = [0.0,0.0,0.0])
+parser.set_defaults(grid = [0,0,0])
+parser.set_defaults(size  = [0.0,0.0,0.0])
 parser.set_defaults(twoD = False)
 
 (options, filenames) = parser.parse_args()
@@ -86,7 +87,7 @@ for file in files:
   m = re.search('(\d+)\s*head', firstline.lower())
   if m:
     headerlines = int(m.group(1))
-    headers  = [firstline]+[file['input'].readline() for i in range(headerlines)]
+    headers  = [file['input'].readline() for i in range(headerlines)]
   else:
     headerlines = 1
     headers = firstline
@@ -94,15 +95,20 @@ for file in files:
   content = file['input'].readlines()
   file['input'].close()
 
-  info = {'resolution': numpy.array(options.resolution),
-          'dimension':  numpy.array(options.dimension),
-          'origin':     numpy.array([0.0,0.0,0.0]),
-          'homogenization': 1,
-         }
+  info = {
+        'grid':   numpy.array(options.grid),
+        'size':   numpy.array(options.size),
+        'origin': numpy.zeros(3,'d'),
+        'microstructures': 0,
+        'homogenization':  0
+       }
 
   new_header = []
+  new_header.append('$Id$\n')
   for header in headers:
     headitems = map(str.lower,header.split())
+    if headitems[0] == 'resolution': headitems[0] = 'grid'
+    if headitems[0] == 'dimension':  headitems[0] = 'size'
     if headitems[0] in mappings.keys():
       if headitems[0] in identifiers.keys():
         for i in xrange(len(identifiers[headitems[0]])):
@@ -110,49 +116,50 @@ for file in files:
             mappings[headitems[0]](headitems[headitems.index(identifiers[headitems[0]][i])+1])
       else:
         info[headitems[0]] = mappings[headitems[0]](headitems[1])
+    else:
+      new_header.append(header)
     
-  if numpy.all(info['resolution'] == 0):
-    file['croak'].write('no resolution info found.\n')
+  if numpy.all(info['grid'] == 0):
+    file['croak'].write('no grid info found.\n')
     continue
-  if numpy.all(info['dimension'] == 0.0):
-    file['croak'].write('no dimension info found.\n')
+  if numpy.all(info['size'] == 0.0):
+    file['croak'].write('no size info found.\n')
     continue
 
-  if options.resolution == [0,0,0]:
-    options.resolution = info['resolution']
-  if options.dimension == [0.0,0.0,0.0]:
-    options.dimension = info['dimension']
-
-  file['croak'].write('resolution:     %s\n'%(' x '.join(map(str,info['resolution']))) + \
-                      'dimension:      %s\n'%(' x '.join(map(str,info['dimension']))) + \
-                      'origin:         %s\n'%(' : '.join(map(str,info['origin']))) + \
-                      'homogenization: %i\n'%info['homogenization'])
-
-  new_header.append("resolution\ta %i\tb %i\tc %i\n"%( 
-    options.resolution[0],
-    options.resolution[1],
-    options.resolution[2],))
-  new_header.append("dimension\tx %f\ty %f\tz %f\n"%(
-    options.dimension[0],
-    options.dimension[1],
-    options.dimension[2],))
-  new_header.append("origin\tx %f\ty %f\tz %f\n"%(
-    info['origin'][0],
-    info['origin'][1],
-    info['origin'][2],))
-  new_header.append("homogenization\t%i\n"%info['homogenization'])
+  file['croak'].write('-- input --\n' +\
+                      'grid     a b c:  %s\n'%(' x '.join(map(str,info['grid']))) + \
+                      'size     x y z:  %s\n'%(' x '.join(map(str,info['size']))) + \
+                      'origin   x y z:  %s\n'%(' : '.join(map(str,info['origin']))) + \
+                      'homogenization:  %i\n'%info['homogenization'] + \
+                      'microstructures: %i\n'%info['microstructures'])
+                      
+  if options.grid == [0,0,0]:
+    options.grid = info['grid']
+  if options.size == [0.0,0.0,0.0]:
+    options.size = info['size']
     
-  microstructure = numpy.zeros(info['resolution'],'i')
+  microstructure = numpy.zeros(info['grid'],'i')
   i = 0
   for line in content:  
     for item in map(int,line.split()):
-      microstructure[i%info['resolution'][0],
-                    (i/info['resolution'][0])%info['resolution'][1],
-                     i/info['resolution'][0] /info['resolution'][1]] = item
+      microstructure[i%info['grid'][0],
+                    (i/info['grid'][0])%info['grid'][1],
+                     i/info['grid'][0] /info['grid'][1]] = item
       i += 1
-  
+  info['microstructures'] = microstructure.max()
   formatwidth = 1+int(math.floor(math.log10(microstructure.max())))
-           
+  
+  file['croak'].write('-- output --\n' +\
+                      'grid     a b c:  %s\n'%(' x '.join(map(str,options.grid))) + \
+                      'size     x y z:  %s\n'%(' x '.join(map(str,options.size)))  + \
+                      'microstructures: %i\n'%info['microstructures'])
+
+  new_header.append("grid\ta %i\tb %i\tc %i\n"%(options.grid[0],options.grid[1],options.grid[2],))
+  new_header.append("size\tx %f\ty %f\tz %f\n"%(options.size[0],options.size[1],options.size[2],))
+  new_header.append("origin\tx %f\ty %f\tz %f\n"%(info['origin'][0],info['origin'][1],info['origin'][2],))
+  new_header.append("microstructures\t%i\n"%info['microstructures'])
+  new_header.append("homogenization\t%i\n"%info['homogenization'])
+  
 # ------------------------------------------ assemble header ---------------------------------------  
 
   output  = '%i\theader\n'%(len(new_header))
@@ -160,12 +167,12 @@ for file in files:
 
 # ------------------------------------- regenerate texture information ----------------------------------  
 
-  for c in xrange(options.resolution[2]):
-    z = int(info['resolution'][2]*(c+0.5)/options.resolution[2])%info['resolution'][2]
-    for b in xrange(options.resolution[1]):
-      y = int(info['resolution'][1]*(b+0.5)/options.resolution[1])%info['resolution'][1]
-      for a in xrange(options.resolution[0]):
-        x = int(info['resolution'][0]*(a+0.5)/options.resolution[0])%info['resolution'][0]
+  for c in xrange(options.grid[2]):
+    z = int(info['grid'][2]*(c+0.5)/options.grid[2])%info['grid'][2]
+    for b in xrange(options.grid[1]):
+      y = int(info['grid'][1]*(b+0.5)/options.grid[1])%info['grid'][1]
+      for a in xrange(options.grid[0]):
+        x = int(info['grid'][0]*(a+0.5)/options.grid[0])%info['grid'][0]
         output += str(microstructure[x,y,z]).rjust(formatwidth) + {True:' ',False:'\n'}[options.twoD]
       output += {True:'\n',False:''}[options.twoD]
     
