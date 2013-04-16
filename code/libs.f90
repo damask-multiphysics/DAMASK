2ba986be2ab7dedb86dcde46ddbf548b5ad56d11
Martin	Diehl <m.diehl@mpie.de>
2013-04-16 12:45:12 +0000

    introduced new dummy module "libs" for inclusion of further library sources. contains kdtree2 and Lib_VTK_IO at and is enabled for the spectral solver only at the moment

---
+++ code/libs.f90
@@ -0,0 +1,33 @@
+! Copyright 2011-13 Max-Planck-Institut für Eisenforschung GmbH
+!
+! This file is part of DAMASK,
+! the Düsseldorf Advanced Material Simulation Kit.
+!
+! DAMASK is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! DAMASK is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with DAMASK. If not, see <http://www.gnu.org/licenses/>.
+!
+!--------------------------------------------------------------------------------------------------
+! $Id$
+!--------------------------------------------------------------------------------------------------
+!> @author Martin Diehl, Max-Planck-Institut für Eisenforschung GmbH
+!> @brief dummy source for inclusion of Library files
+!--------------------------------------------------------------------------------------------------
+
+#ifdef Spectral
+#include "kdtree2.f90"
+#include "IR_Precision.f90"
+#include "Lib_VTK_IO.f90"
+#endif
+
+module libs
+end module libs
--- code/math.f90
+++ code/math.f90
@@ -16,9 +16,6 @@
 ! You should have received a copy of the GNU General Public License
 ! along with DAMASK. If not, see <http://www.gnu.org/licenses/>.
 !
-#ifdef Spectral
-#include "kdtree2.f90"
-#endif
 !--------------------------------------------------------------------------------------------------
 ! $Id$
 !--------------------------------------------------------------------------------------------------
