f02ee16b3da1f64b03037f799c54e0f219a115e4
Philip	Eisenlohr <eisenlohr@egr.msu.edu>
2013-09-09 14:09:38 +0000

    fixed missing comma in "pre" symlinking list.

--- processing/setup/symLink_Processing.py
+++ processing/setup/symLink_Processing.py
@@ -19,7 +19,7 @@ bin_link = { \
                 'randomSeeding.py',
                 'geom_fromAng.py',
                 'geom_fromVPSC.py',
-                'geom_fromEuclideanDistance.py'
+                'geom_fromEuclideanDistance.py',
                 'geom_fromMinimalSurface.py',
                 'geom_fromVoronoiTessellation.py',
                 'geom_fromOsteonGeometry.py',
@@ -31,6 +31,7 @@ bin_link = { \
                 'geom_translate.py',
                 'geom_vicinityOffset.py',
                 'geom_grainGrowth.py',
+                'geom_poke.py',
                 ],
         'post' : [
                 '3Dvisualize.py',
