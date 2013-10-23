96b03841e88768be25248ae1e684b7432ee564f3
Martin	Diehl <m.diehl@mpie.de>
2013-10-23 08:38:14 +0000

    redefined macro for intel compiler, should work with windows (but Nicolo had some problems) made configure verbose

--- code/compilation_info.f90
+++ code/compilation_info.f90
@@ -22,9 +22,9 @@
     write(6,*) 'Compiled with ', compiler_version() !not supported by GFORTRAN 4.5 and ifort 12
     write(6,*) 'With options  ', compiler_options()
 #endif 
-#ifdef _INTEL_COMPILER
-  write(6,'(a,i4.4,a,i8.8)') ' Compiled with Intel fortran version ', _INTEL_COMPILER,&
-                                                        ', build date ', _INTEL_COMPILER_BUILD_DATE
+#ifdef __INTEL_COMPILER
+  write(6,'(a,i4.4,a,i8.8)') ' Compiled with Intel fortran version ', __INTEL_COMPILER,&
+                                                        ', build date ', __INTEL_COMPILER_BUILD_DATE
 #endif
 write(6,*) 'Compiled on ', __DATE__,' at ',__TIME__
 write(6,*)
