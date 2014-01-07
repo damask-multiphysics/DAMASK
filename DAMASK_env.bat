2a5443d41a06599820f66b1c0821a748b0a50787
Martin	Diehl <m.diehl@mpie.de>
2014-01-07 23:04:00 +0000

    added correct end of line style, bat file for windows should work now

--- DAMASK_env.bat
+++ DAMASK_env.bat
@@ -1,16 +1,11 @@
 :: sets up an environment for DAMASK on Windows
-:: usage:  ????
+:: usage:  call DAMASK_env.bat
 @echo off
+set LOCATION=%~dp0
+set DAMASK_ROOT=%LOCATION%\DAMASK
+set DAMASK_NUM_THREADS=2
 chcp 1252
 Title Düsseldorf Advanced Materials Simulation Kit - DAMASK, MPIE Düsseldorf
-
-SET DAMASK_ROOT=%HOME%\DAMASK                                                                     
-SET DAMASK_BIN=%DAMASK_ROOT%\bin
-SET PYTHONPATH=%PYTHONPATH%:%DAMASK_ROOT%\lib
-SET DAMASK_NUM_THREADS=2
-
-
-
 echo.
 echo Düsseldorf Advanced Materials Simulation Kit - DAMASK
 echo Max-Planck-Institut für Eisenforschung, Düsseldorf
@@ -19,3 +14,6 @@ echo.
 echo Preparing environment ...
 echo DAMASK_ROOT=%DAMASK_ROOT%
 echo DAMASK_NUM_THREADS=%DAMASK_NUM_THREADS%
+set DAMASK_BIN=%DAMASK_ROOT%\bin
+set PATH=%PATH%;%DAMASK_BIN%
+set PYTHONPATH=%PYTHONPATH%;%DAMASK_ROOT%\lib
