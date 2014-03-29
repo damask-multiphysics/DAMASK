4bb5439994025901e0d6ce2513fb74c3cd066f80
Martin	Diehl <m.diehl@mpie.de>
2014-03-29 08:20:36 +0000

    removed copyright from *.f90 and *.f files, will be added using the deployMe script for release/nightly build. Now based on presence of svn:keywords property set (usually "Id"). Similar thing will be done for .py, .sh, and .config files but more involved because they might be executable

--- code/core_quit.f90
+++ code/core_quit.f90
@@ -1,40 +1,8 @@
-! Copyright 2011-13 Max-Planck-Institut für Eisenforschung GmbH
-!
-! This file is part of DAMASK,
-! the Düsseldorf Advanced MAterial Simulation Kit.
-!
-! DAMASK is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by
-! the Free Software Foundation, either version 3 of the License, or
-! (at your option) any later version.
-!
-! DAMASK is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-! GNU General Public License for more details.
-!
-! You should have received a copy of the GNU General Public License
-! along with DAMASK. If not, see <http://www.gnu.org/licenses/>.
-!
 !##################################################################################################
-!* $Id$
+! $Id$
 !##################################################################################################
-! Material subroutine for BVP solution using spectral method
-!
-! Run 'DAMASK_spectral.exe --help' to get usage hints
-!
-! written by P. Eisenlohr,
-!            F. Roters,
-!            L. Hantcherli,
-!            W.A. Counts,
-!            D.D. Tjahjanto,
-!            C. Kords,
-!            M. Diehl,
-!            R. Lebensohn
-!
-! MPI fuer Eisenforschung, Duesseldorf
 !********************************************************************
-! quit subroutine to satisfy IO_error
+! quit subroutine to satisfy IO_error for core module
 !
 !********************************************************************
 subroutine quit(stop_id)
--- code/debug.f90
+++ code/debug.f90
@@ -1,23 +1,5 @@
-! Copyright 2011-2013 Max-Planck-Institut für Eisenforschung GmbH
-!
-! This file is part of DAMASK,
-! the Düsseldorf Advanced Material Simulation Kit.
-!
-! DAMASK is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by
-! the Free Software Foundation, either version 3 of the License, or
-! (at your option) any later version.
-!
-! DAMASK is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-! GNU General Public License for more details.
-!
-! You should have received a copy of the GNU General Public License
-! along with DAMASK. If not, see <http://www.gnu.org/licenses/>.
-!
 !--------------------------------------------------------------------------------------------------
-!* $Id$
+! $Id$
 !--------------------------------------------------------------------------------------------------
 !> @author Franz Roters, Max-Planck-Institut für Eisenforschung GmbH
 !> @author Philip Eisenlohr, Max-Planck-Institut für Eisenforschung GmbH
