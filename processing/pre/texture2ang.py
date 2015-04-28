afe7225357edd2cad49d80d96f2cb73965789d86
Haiming	Zhang <hm.zhang@sjtu.edu.cn>
2015-04-28 18:09:07 +0000

    add the option of exporting '.ctf' format file.

--- processing/pre/texture2ang.py
+++ processing/pre/texture2ang.py
@@ -22,50 +22,67 @@ def positiveRadians(angle):
 
   return angle
 
-
-def getHeader(sizeX,sizeY,step):
-  
-  return [ 
-  '# TEM_PIXperUM          1.000000', 
-  '# x-star                0.509548', 
-  '# y-star                0.795272', 
-  '# z-star                0.611799', 
-  '# WorkingDistance       18.000000', 
-  '#', 
-  '# Phase                 1', 
-  '# MaterialName          Al', 
-  '# Formula               Fe', 
-  '# Info', 
-  '# Symmetry              43', 
-  '# LatticeConstants      2.870 2.870 2.870  90.000  90.000  90.000', 
-  '# NumberFamilies        4', 
-  '# hklFamilies           1  1  0 1 0.000000 1', 
-  '# hklFamilies           2  0  0 1 0.000000 1', 
-  '# hklFamilies           2  1  1 1 0.000000 1', 
-  '# hklFamilies           3  1  0 1 0.000000 1', 
-  '# Categories            0 0 0 0 0 ', 
-  '#', 
-  '# GRID: SquareGrid', 
-  '# XSTEP: ' + str(step), 
-  '# YSTEP: ' + str(step), 
-  '# NCOLS_ODD: ' + str(sizeX), 
-  '# NCOLS_EVEN: ' + str(sizeX), 
-  '# NROWS: ' + str(sizeY), 
-  '#', 
-  '# OPERATOR: ODFsammpling', 
-  '#', 
-  '# SAMPLEID: ', 
-  '#', 
-  '# SCANID: ', 
-  '#'
-  ]
+def getHeader(sizeX,sizeY,step,format):
+  if format == 'ang':
+    return [ 
+    '# TEM_PIXperUM          1.000000', 
+    '# x-star                0.509548', 
+    '# y-star                0.795272', 
+    '# z-star                0.611799', 
+    '# WorkingDistance       18.000000', 
+    '#', 
+    '# Phase                 1', 
+    '# MaterialName          Al', 
+    '# Formula               Fe', 
+    '# Info', 
+    '# Symmetry              43', 
+    '# LatticeConstants      2.870 2.870 2.870  90.000  90.000  90.000', 
+    '# NumberFamilies        4', 
+    '# hklFamilies           1  1  0 1 0.000000 1', 
+    '# hklFamilies           2  0  0 1 0.000000 1', 
+    '# hklFamilies           2  1  1 1 0.000000 1', 
+    '# hklFamilies           3  1  0 1 0.000000 1', 
+    '# Categories            0 0 0 0 0 ', 
+    '#', 
+    '# GRID: SquareGrid', 
+    '# XSTEP: ' + str(step), 
+    '# YSTEP: ' + str(step), 
+    '# NCOLS_ODD: ' + str(sizeX), 
+    '# NCOLS_EVEN: ' + str(sizeX), 
+    '# NROWS: ' + str(sizeY), 
+    '#', 
+    '# OPERATOR: ODFsammpling', 
+    '#', 
+    '# SAMPLEID: ', 
+    '#', 
+    '# SCANID: ', 
+    '#'
+    ]
+  else:  # ctf format
+    return [ \
+    'Channel Text File',
+    'Prj      X:\\xxx\\xxxx.cpr',
+    'Author   [MPIE-DAMASK]',
+    'JobMode  Grid',
+    'XCells   '+ str(sizeX),
+    'YCells   '+ str(sizeY),
+    'XStep    '+ str(step),
+    'YStep    '+ str(step),
+    'AcqE1    0',
+    'AcqE2    90',
+    'AcqE3    0',
+    'Euler angles refer to Sample Coordinate system (CS0)! Mag 300  Coverage 100  Device 0  KV 20  TiltAngle 70  TiltAxis 0',
+    'Phases   1',
+    '4.05;4.05;4.05  90;90;90  Aluminium  11  225  3803863129_5.0.6.3    -2102160418    Cryogenics18,54-55',
+    'Phase X      Y Bands Error Euler1     Euler2     Euler3    MAD    BC BS'
+    ]
 
 
 # --------------------------------------------------------------------
 #                                MAIN
 # --------------------------------------------------------------------
 parser = OptionParser(option_class=damask.extendableOption, usage='%prog options [file[s]]', description = """
-Transform linear binned data into Euler angles.
+output the Euler angles in the format supported by TSL (.ang or .ctf).
 
 """, version = scriptID)
 
@@ -73,9 +90,12 @@ parser.add_option("-c", "--column", type="int", dest="column",
                   help="starting column of Euler triplet")
 parser.add_option("-s", "--skip",   type="int", dest="skip",
                   help="skip this many lines of heading info [%default]")
+parser.add_option("-f", "--format", type="string", dest="format",
+                  help="the format of the output file [%default]")
 
 parser.set_defaults (column = 1)
 parser.set_defaults (skip = 1)
+parser.set_defaults (format = 'ang')
 
 (options,filenames) = parser.parse_args()
 options.column -= 1
@@ -104,20 +124,30 @@ for file in files:
   if m != None and options.skip == 0: options.skip = int(m.group(1))+1
 
   # extract orientation angles
-  angles = [map(positiveRadians,line.split()[options.column:options.column+3]) for line in content[options.skip:]]
+  if options.format == 'ang': # 'ang' file, radian
+    angles = [map(positiveRadians,line.split()[options.column:options.column+3]) for line in content[options.skip:]]
+  else:                       # 'ctf' file, degree
+    angles = [line.split()[options.column:options.column+3] for line in content[options.skip:]]
 
   nPoints = len(angles)
   sizeY = integerFactorization(nPoints)
   sizeX = nPoints / sizeY
   file['croak'].write('%s: %i*%i = %i (== %i)\n'%(file['name'],sizeX,sizeY,sizeX*sizeY,nPoints) )
 
-  # write ang file
-  for line in getHeader(sizeX,sizeY,1.0):
+  # write ang/ctf file
+  for line in getHeader(sizeX,sizeY,1.0,options.format):
     file['output'].write(line + '\n')
   for counter,point in enumerate(angles):
-    file['output'].write(''.join(['%10.5f'%angle for angle in point])+
-                         ''.join(['%10.5f'%coord for coord in [counter%sizeX,counter//sizeX]])+
-                         ' 100.0 1.0 0 1 1.0\n')
+    if options.format == 'ang': 
+      file['output'].write(''.join(['%10.5f'%angle for angle in point])+
+                           ''.join(['%10.5f'%coord for coord in [counter%sizeX,counter//sizeX]])+
+                           ' 100.0 1.0 0 1 1.0\n')
+    else:
+      file['output'].write('1  '+
+                           ' '.join(['%6.1f'%coord for coord in [counter%sizeX,counter//sizeX]])+
+                           '  5  0 '+
+                           ' '.join(['%10.5f'%float(angle) for angle in point])+
+                           '  0.5000  100  0\n')
   if file['name'] != 'STDIN':
      file['output'].close()
-     os.rename(file['name']+'_tmp', os.path.splitext(file['name'])[0]+'.ang' )
+     os.rename(file['name']+'_tmp', os.path.splitext(file['name'])[0]+'.'+options.format )
