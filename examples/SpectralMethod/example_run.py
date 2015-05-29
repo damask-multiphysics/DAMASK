73d97d3d71c7ecf8ead916630c2292b0ac5e6223
Philip	Eisenlohr <eisenlohr@egr.msu.edu>
2015-05-29 19:50:59 +0000

    added command line arguments to select resolution (defaults to 16)

--- examples/SpectralMethod/example_run.py
+++ examples/SpectralMethod/example_run.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 no BOM -*-
+import sys
 
+resolutions = [16,32,64]
+resolution = resolutions[0]
+
+try:
+  resolution = int(sys.argv[1])
+except:
+  pass
+
+if resolution not in resolutions:
+  resolution = resolutions[0]
+  
 from subprocess import call
-call('make run16x16x16', shell=True)
+call('make run%s'%('x'.join([str(resolution)]*3)), shell=True)
