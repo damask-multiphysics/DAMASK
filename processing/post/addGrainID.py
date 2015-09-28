202ee00049129c79e8cf49db4799753ff040219d
Harsha	Phukan <phykanha@egr.msu.edu>
2015-09-28 19:03:24 +0000

    Eliminated redundant searches for similar orientations in neighboring points…speeds up script by 2 orders of magnitude

--- processing/post/addGrainID.py
+++ processing/post/addGrainID.py
@@ -193,7 +193,7 @@ for name in filenames:
         gID = grainID[i]
         if gID != -1 and gID not in alreadyChecked:                                                 # an already indexed point belonging to a grain not yet tested?
           alreadyChecked[gID] = True                                                                # remember not to check again
-          disorientation = o.disorientation(orientations[gID])[0]                                   # compare against that grain's orientation
+          disorientation = o.disorientation(orientations[gID],strict = False)[0]                    # compare against that grain's orientation [Don't search for axes falling into SST!]
           if disorientation.quaternion.w >  cos_disorientation and \
              disorientation.quaternion.w >= bestDisorientation.w:                                   # within disorientation threshold and better than current best?
             matched = True
@@ -217,11 +217,11 @@ for name in filenames:
   memberCounts = np.array(memberCounts)
   similarOrientations = [[] for i in xrange(len(orientations))]
 
-  for i,orientation in enumerate(orientations[:-1]):                                                # compare each identified orientation...
-    for j in xrange(i+1,len(orientations)):                                                         # ...against all others that were defined afterwards
-      if orientation.disorientation(orientations[j])[0].quaternion.w > cos_disorientation:          # similar orientations in both grainIDs?
-        similarOrientations[i].append(j)                                                            # remember in upper triangle...
-        similarOrientations[j].append(i)                                                            # ...and lower triangle of matrix
+  for i,orientation in enumerate(orientations[:-1]):                                                       # compare each identified orientation...
+    for j in xrange(i+1,len(orientations)):                                                                # ...against all others that were defined afterwards
+      if orientation.disorientation(orientations[j],strict=False)[0].quaternion.w > cos_disorientation:    # similar orientations in both grainIDs?
+        similarOrientations[i].append(j)                                                                   # remember in upper triangle...
+        similarOrientations[j].append(i)                                                                   # ...and lower triangle of matrix
 
     if similarOrientations[i] != []:
       bg.set_message('grainID {} is as: {}'.format(i,' '.join(map(str,similarOrientations[i]))))
