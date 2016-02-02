08150564cf045b3b3fc75d2329c4866c94460ca4
Martin Diehl <m.diehl@mpie.de>
2016-02-02 23:34:42 +0100

    added info like in spectral solver

--- code/DAMASK_abaqus_std.f
+++ code/DAMASK_abaqus_std.f
@@ -35,8 +35,17 @@ contains
 !> @brief just reporting 
 !--------------------------------------------------------------------------------------------------
 subroutine DAMASK_interface_init
-
+ integer, dimension(8) :: &
+   dateAndTime                                                                                      ! type default integer
+ call date_and_time(values = dateAndTime)
  write(6,'(/,a)') ' <<<+-  DAMASK_abaqus_std  -+>>>'
+ write(6,'(/,a)')              ' Version: '//DAMASKVERSION
+ write(6,'(a,2(i2.2,a),i4.4)') ' Date:    ',dateAndTime(3),'/',&
+                                            dateAndTime(2),'/',&
+                                            dateAndTime(1) 
+ write(6,'(a,2(i2.2,a),i2.2)') ' Time:    ',dateAndTime(5),':',&
+                                            dateAndTime(6),':',&
+                                            dateAndTime(7)  
  write(6,'(/,a)') ' <<<+-  DAMASK_interface init  -+>>>'
 #include "compilation_info.f90"  
 
