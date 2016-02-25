a8f0624849c2e7023b3a51770b64a103d5e1895f
zhangc43 <zhangc43@egr.msu.edu>
2016-02-25 16:40:26 -0500

    initial translation is done for spectral solver

--- build_spectral.sh
+++ build_spectral.sh
@@ -2,7 +2,7 @@
 
 cat  README
 echo "Building spectral solver with ${FC}"
-DAMASKVERSION :=$(shell cat ../VERSION)
+DAMASKVERSION :=$(sh cat ../VERSION)
 
 # prepare building directory
 if [ -d build_spectral ] ; then
@@ -15,14 +15,12 @@ cd build_spectral
 # CMake call
 # PETSC_DIR         |  PETSC directory
 # HDF5_DIR          |  HDF5 library (same compiler for DAMASK)
-# DAMASK_ROOT       |  DAMASK source location
 # DAMASK_V          |  DAMASK current revision
 # CMAKE_BUILD_TYPE  |  Default set to release (no debugging output)
 # OPENMP            |  "ON" will turn on OPENMP support
 # OPTIMIZATION      |  [OFF,DEFENSIVE,AGGRESSIVE,ULTRA]
 cmake -D PETSC_DIR=${PETSC_DIR}    \
       -D HDF5_DIR=${HDF5_DIR}      \
-      -D DAMASK_ROOT=..            \
       -D DAMASK_V=${DAMASKVERSION} \
       -D CMAKE_BUILD_TYPE=RELEASE  \
       -D OPENMP=ON                 \
