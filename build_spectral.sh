0e34d0468c7090a7697b72945acae9089887fc5d
Chen Zhang <zhangc43@baron.egr.msu.edu>
2016-03-04 11:18:38 -0500

    adding install target in CMake

--- build_spectral.sh
+++ build_spectral.sh
@@ -23,18 +23,25 @@ cd build_spectral
 
 ##
 # CMake call
-# PETSC_DIR         |  PETSC directory
-# HDF5_DIR          |  HDF5 library (same compiler for DAMASK)
-# DAMASK_V          |  DAMASK current revision
-# CMAKE_BUILD_TYPE  |  Default set to release (no debugging output)
-# OPENMP            |  [ON/OFF]
-# OPTIMIZATION      |  [OFF,DEFENSIVE,AGGRESSIVE,ULTRA]
-# DAMASK_DRIVER     |  [SPECTRAL, FEM]
-cmake -D PETSC_DIR=${PETSC_DIR}    \
-      -D HDF5_DIR=${HDF5_DIR}      \
-      -D DAMASK_V=${DAMASKVERSION} \
-      -D CMAKE_BUILD_TYPE=RELEASE  \
-      -D OPENMP=ON                 \
-      -D OPTIMIZATION=DEFENSIVE    \
-      -D DAMASK_DRIVER=SPECTRAL    \
+# PETSC_DIR          |  PETSC directory
+# HDF5_DIR           |  HDF5 library (same compiler for DAMASK)
+# DAMASK_V           |  DAMASK current revision
+# CMAKE_BUILD_TYPE   |  Default set to release (no debugging output)
+# OPENMP             |  [ON/OFF]
+# OPTIMIZATION       |  [OFF,DEFENSIVE,AGGRESSIVE,ULTRA]
+# DAMASK_DRIVER      |  [SPECTRAL, FEM]
+# DAMASK_INSTALL     |  Directory to install binary output
+cmake -D PETSC_DIR=${PETSC_DIR}     \
+      -D DAMASK_V=${DAMASKVERSION}  \
+      -D CMAKE_BUILD_TYPE=RELEASE   \
+      -D OPENMP=ON                  \
+      -D OPTIMIZATION=DEFENSIVE     \
+      -D DAMASK_DRIVER=SPECTRAL     \
+      -D DAMASK_INSTALL=${HOME}/bin \
       ../..
+
+echo
+echo "Please move to the build directory using"
+echo "    cd build/build_spectral"
+echo "Using the following command to build DAMASK spectral solver"
+echo "    make clean all install"
