280076e7ef1894c682c361f4b5214eb67f3b4e8b
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2016-05-11 10:04:01 -0400

    removed (probably obsolete) warning about missing h5py module.

--- lib/damask/result.py
+++ lib/damask/result.py
@@ -8,7 +8,7 @@ import sys
 try:
   import h5py
 except (ImportError) as e:
-  sys.stderr.write('\nREMARK: h5py module not available \n\n')
+  pass #  sys.stderr.write('\nREMARK: h5py module not available \n\n')
 
 class Result():
   """
