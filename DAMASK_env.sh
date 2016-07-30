7de1899884133b3d2c8f9bda4153428c69ee69fc
Martin Diehl <m.diehl@mpie.de>
2016-07-30 08:53:42 +0200

    print statement not existing in python3, use print function

--- DAMASK_env.sh
+++ DAMASK_env.sh
@@ -73,16 +73,16 @@ if [ ! -z "$PS1" ]; then
    || echo $(python -c \
           "import math; \
            size=$(( 1024*$(ulimit -d) )); \
-           print '{:.4g} {}'.format(size / (1 << ((int(math.log(size,2) / 10) if size else 0) * 10)), \
-           ['bytes','KiB','MiB','GiB','TiB','EiB','ZiB'][int(math.log(size,2) / 10) if size else 0])")
+           print('{:.4g} {}'.format(size / (1 << ((int(math.log(size,2) / 10) if size else 0) * 10)), \
+           ['bytes','KiB','MiB','GiB','TiB','EiB','ZiB'][int(math.log(size,2) / 10) if size else 0]))")
   echo -n "stack size         "
    [[ "$(ulimit -s)" == "unlimited" ]] \
    && echo "unlimited" \
    || echo $(python -c \
           "import math; \
            size=$(( 1024*$(ulimit -s) )); \
-           print '{:.4g} {}'.format(size / (1 << ((int(math.log(size,2) / 10) if size else 0) * 10)), \
-           ['bytes','KiB','MiB','GiB','TiB','EiB','ZiB'][int(math.log(size,2) / 10) if size else 0])")
+           print('{:.4g} {}'.format(size / (1 << ((int(math.log(size,2) / 10) if size else 0) * 10)), \
+           ['bytes','KiB','MiB','GiB','TiB','EiB','ZiB'][int(math.log(size,2) / 10) if size else 0]))")
 fi
 
 export DAMASK_NUM_THREADS
