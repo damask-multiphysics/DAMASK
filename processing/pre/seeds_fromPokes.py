9bb4f89e78bf425df74902c5cbfded19e5f94ef9
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2016-12-06 21:36:45 -0500

    removed leftover debug output

--- processing/pre/seeds_fromPokes.py
+++ processing/pre/seeds_fromPokes.py
@@ -109,7 +109,6 @@ for name in filenames:
     for j in range(Ny):
       grid[0] = round((i+0.5)*box[0]*info['grid'][0]/Nx-0.5)+offset[0]
       grid[1] = round((j+0.5)*box[1]*info['grid'][1]/Ny-0.5)+offset[1]
-      damask.util.croak('x,y coord on surface: {},{}...'.format(*grid[:2]))
       for k in range(Nz):
         grid[2] = k + offset[2]
         grid %= info['grid']
