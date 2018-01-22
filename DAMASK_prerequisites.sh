136df3618d89e836e14fddd0d47d7078675fcb1a
Martin Diehl <m.diehl@mpie.de>
2018-01-22 22:55:56 +0100

    improving prerequisites report for next release

--- DAMASK_prerequisites.sh
+++ DAMASK_prerequisites.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 #==================================================================================================
-# Execute this script and send system_report.txt to damask@mpie.de for support
+# Execute this script (type './DAMASK_prerequisites.sh') 
+# and send system_report.txt to damask@mpie.de for support
 #==================================================================================================
 
 OUTFILE="system_report.txt"
-echo generating $OUTFILE
+echo ===========================================
+echo +  Generating $OUTFILE                    
+echo +  Send to damask@mpie.de for support
+echo ===========================================
 
-echo date +"%m-%d-%y" >$OUTFILE
+
 
 # redirect STDOUT and STDERR to logfile
 # https://stackoverflow.com/questions/11229385/redirect-all-output-in-a-bash-script-when-using-set-x^
@@ -17,6 +21,10 @@ exec > $OUTFILE 2>&1
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DAMASK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo System report for \'$(hostname)\' created on $(date '+%Y-%m-%d %H:%M:%S') by \'$(whoami)\'
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo
 echo ==============================================================================================
 echo DAMASK settings
 echo ==============================================================================================
@@ -34,19 +42,24 @@ echo System
 echo ==============================================================================================
 uname -a
 echo
+echo PATH: $PATH
+echo LD_LIBRARY_PATH: $LD_LIBRARY_PATH
+echo PYTHONPATH: $PYTHONPATH
+echo SHELL: $SHELL
+echo 
 echo ==============================================================================================
 echo Python
 echo ==============================================================================================
 
 DEFAULT_PYTHON=python2.7
 for executable in python python2 python3 python2.7; do
-  if [[ "$(which $executable)x" != "x" ]]; then
-     echo $executable version: $($executable --version 2>&1)
+  if which $executable &> /dev/null; then
+    echo $executable version: $($executable --version 2>&1)
   else
-     echo $executable does not exist
+    echo $executable does not exist
   fi
 done
-echo Location of $DEFAULT_PYTHON: $(ls -la $(which $DEFAULT_PYTHON))
+echo Details on $DEFAULT_PYTHON: $(ls -la $(which $DEFAULT_PYTHON))
 echo
 for module in numpy scipy;do
   echo ----------------------------------------------------------------------------------------------
@@ -73,7 +86,7 @@ echo ===========================================================================
 echo GCC
 echo ==============================================================================================
 for executable in gcc g++ gfortran ;do
-  if [[ "$(which $executable)x" != "x" ]]; then
+  if which $executable &> /dev/null; then
     echo $(which $executable) version: $($executable --version 2>&1)
   else
      echo $executable does not exist
@@ -84,10 +97,10 @@ echo ===========================================================================
 echo Intel Compiler Suite
 echo ==============================================================================================
 for executable in icc icpc ifort ;do
-  if [[ "$(which $executable)x" != "x" ]]; then
+  if which $executable &> /dev/null; then
     echo $(which $executable) version: $($executable --version 2>&1)
   else
-     echo $executable does not exist
+    echo $executable does not exist
   fi
 done
 echo
@@ -95,7 +108,7 @@ echo ===========================================================================
 echo MPI Wrappers
 echo ==============================================================================================
 for executable in mpicc mpiCC mpicxx mpicxx mpifort mpif90 mpif77; do
-  if [[ "$(which $executable)x" != "x" ]]; then
+  if which $executable &> /dev/null; then
     echo $(which $executable) version: $($executable --show 2>&1)
   else
      echo $executable does not exist
