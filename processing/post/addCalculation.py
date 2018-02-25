b07823464477b1fadf4f7897eaeec1130c27ccaf
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2018-02-25 14:16:55 -0500

    added scipy and scipy.linalg as known modules for calculations

--- processing/post/addCalculation.py
+++ processing/post/addCalculation.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 no BOM -*-
 
 import os,re,sys,collections
-import math                                                                                         # noqa
+import math,scipy,scipy.linalg                                                                     # noqa
 import numpy as np
 from optparse import OptionParser
 import damask
