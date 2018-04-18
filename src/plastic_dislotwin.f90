71d23274e744a2f8d561439c45227b46fde8464e
Sharan Roongta <s.roongta@mpie.de>
2018-04-18 15:22:19 +0200

    A citation on twinning was missing

--- src/plastic_dislotwin.f90
+++ src/plastic_dislotwin.f90
@@ -269,6 +269,8 @@ subroutine plastic_dislotwin_init(fileUnit)
  write(6,'(/,a)')   ' https://doi.org/10.1016/j.actamat.2004.04.012'
  write(6,'(/,a)')   ' F.Roters et. al , Computational Materials Science, 39:91–95, 2007'
  write(6,'(/,a)')   ' https://doi.org/10.1016/j.commatsci.2006.04.014'
+ write(6,'(/,a)')   ' Wong et.al, Acta Materialia, 118:140–151, 2016'
+ write(6,'(/,a)')   ' https://doi.org/10.1016/j.actamat.2016.07.032'
  write(6,'(a15,a)') ' Current time: ',IO_timeStamp()
 #include "compilation_info.f90"
  
