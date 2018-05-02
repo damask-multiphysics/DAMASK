damask-multiphysics/DAMASK

commit e3bd09d8e69a8d0efa9f7c4899e0af4396c14ae3

wrong initiatilization

src/plastic_phenopowerlaw.f90
@@ -603,7 +603,7 @@ subroutine plastic_phenopowerlaw_init(fileUnit)
      state   (instance)%s_twin=>plasticState(phase)%state   (startIndex:endIndex,:)
      dotState(instance)%s_twin=>plasticState(phase)%dotState(startIndex:endIndex,:)
      plasticState(phase)%state0(startIndex:endIndex,:) = &
-       spread(param(instance)%tau0_twin(1:totalNtwin(instance)),2,NipcMyPhase)
+       spread(math_expand(param(instance)%tau0_twin, param(instance)%Ntwin), 2, NipcMyPhase)
      plasticState(phase)%aTolState(startIndex:endIndex) = param(instance)%aTolResistance
 
      startIndex = endIndex + 1_pInt

commit 74956de9bd4f658ffd954f6d57ded22e7f32d4b5

misplaced bracket caused wrong results introduced in commit e3bd09d8

src/plastic_phenopowerlaw.f90
@@ -856,8 +856,8 @@ subroutine plastic_phenopowerlaw_dotState(Tstar_v,ipc,ip,el)
      right_SlipSlip(j) = abs(1.0_pReal-state(instance)%s_slip(j,of) / &
                                     (param(instance)%tausat_slip(f)+ssat_offset)) &
                          **param(instance)%a_slip&
-                         *sign(1.0_pReal,1.0_pReal-state(instance)%s_slip(j,of)) / &
-                                    (param(instance)%tausat_slip(f)+ssat_offset)
+                         *sign(1.0_pReal,1.0_pReal-state(instance)%s_slip(j,of) / &
+                                    (param(instance)%tausat_slip(f)+ssat_offset))
      right_TwinSlip(j) = 1.0_pReal                                                                  ! no system-dependent part
 
 !--------------------------------------------------------------------------------------------------