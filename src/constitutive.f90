5337fb1229baab5e6a5e18fc3daf2ee0a138981f
Martin Diehl <m.diehl@mpie.de>
2018-08-05 11:08:17 +0200

    old runtime debugging leftovers

--- src/constitutive.f90
+++ src/constitutive.f90
@@ -864,18 +864,10 @@ subroutine constitutive_collectDotState(Tstar_v, FeArray, FpArray, subdt, subfra
    FpArray                                                                                          !< plastic deformation gradient
  real(pReal),  intent(in), dimension(6) :: &
    Tstar_v                                                                                          !< 2nd Piola Kirchhoff stress tensor (Mandel)
- integer(pLongInt) :: &
-   tick = 0_pLongInt, &
-   tock = 0_pLongInt, &
-   tickrate, &
-   maxticks
  integer(pInt) :: &
    ho, &                                                                                            !< homogenization
    tme, &                                                                                           !< thermal member position
    s                                                                                                !< counter in source loop
-
- if (iand(debug_level(debug_constitutive), debug_levelBasic) /= 0_pInt) &
-   call system_clock(count=tick,count_rate=tickrate,count_max=maxticks)
 
  ho  = material_homog(    ip,el)
  tme = thermalMapping(ho)%p(ip,el)
@@ -957,13 +949,6 @@ subroutine constitutive_collectDeltaState(Tstar_v, Fe, ipc, ip, el)
    Fe                                                                                               !< elastic deformation gradient
  integer(pInt) :: &
    s                                                                                                !< counter in source loop
- integer(pLongInt) :: &
-   tick, tock, &
-   tickrate, &
-   maxticks
-
- if (iand(debug_level(debug_constitutive), debug_levelBasic) /= 0_pInt) &
-   call system_clock(count=tick,count_rate=tickrate,count_max=maxticks)
 
  plasticityType: select case (phase_plasticity(material_phase(ipc,ip,el)))
    case (PLASTICITY_KINEHARDENING_ID) plasticityType
