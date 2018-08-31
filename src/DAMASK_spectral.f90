3e811108309c3474f973ccdc56447391642af5c4
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2018-08-31 11:22:59 -0400

    tell user whether DEBUG version or not

--- src/DAMASK_spectral.f90
+++ src/DAMASK_spectral.f90
@@ -154,6 +154,9 @@ program DAMASK_spectral
 ! init DAMASK (all modules)
  call CPFEM_initAll(el = 1_pInt, ip = 1_pInt)
  write(6,'(/,a)')   ' <<<+-  DAMASK_spectral init  -+>>>'
+#ifdef DEBUG
+ write(6,'(/,a)') ' <<<+-     DEBUG version      -+>>>'
+#endif
  write(6,'(/,a,/)') ' Roters et al., Computational Materials Science, 2018'
  write(6,'(a15,a)') ' Current time: ',IO_timeStamp()
 #include "compilation_info.f90"
