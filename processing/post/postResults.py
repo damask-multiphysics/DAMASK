558745b54866e456a52daf55d6cc8b39f7bed387
Martin Diehl <m.diehl@mpie.de>
2018-11-17 08:12:12 +0100

    explicitly require python3
    on older systems, python3 tpyically exists but python2 is the defaul

--- processing/post/postResults.py
+++ processing/post/postResults.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 no BOM -*-
 
 import os,sys,math,re,time,struct
--- processing/pre/hybridIA_linODFsampling.py
+++ processing/pre/hybridIA_linODFsampling.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 no BOM -*-
 
 from optparse import OptionParser
