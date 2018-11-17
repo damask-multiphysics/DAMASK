87a8df3af9b0a2970301a45ab31108c44dc5e479
Martin Diehl <m.diehl@mpie.de>
2018-11-17 12:15:33 +0100

    do not change to "python"
    always specify either "python3" or "python2.7"

--- installation/patch/python2to3.sh
+++ installation/patch/python2to3.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 if [ $1x != 3to2x ]; then
-  echo 'python2.7 to python'
-  find . -name '*.py' -type f | xargs sed -i 's/usr\/bin\/env python2.7/usr\/bin\/env python/g'
+  echo 'python2.7 to python3'
+  find . -name '*.py' -type f | xargs sed -i 's/usr\/bin\/env python2.7/usr\/bin\/env python3/g'
 else
-  echo 'python to python2.7'
-  find . -name '*.py' -type f | xargs sed -i '/2.7/! s/usr\/bin\/env python/usr\/bin\/env python2.7/g'
+  echo 'python3 to python2.7'
+  find . -name '*.py' -type f | xargs sed -i 's/usr\/bin\/env python3/usr\/bin\/env python2.7/g'
 fi
