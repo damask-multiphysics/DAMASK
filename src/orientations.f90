58862a939dfbbeaf4cdd4a72a8670732c8d4c722
Martin Diehl <m.diehl@mpie.de>
2018-12-08 15:54:59 +0100

    initial handling of symmetry introduced

--- src/orientations.f90
+++ src/orientations.f90
@@ -1,8 +1,11 @@
 module orientations
  use rotations
+ use prec, only: &
+   pStringLen
 
  implicit none
  type, extends(rotation), public :: orientation
+   character(len=pStringLen) :: sym = 'none'
  end type orientation
 
  interface orientation
@@ -11,13 +14,16 @@ module orientations
 
 contains
 
-type(orientation) function orientation_init(eu,ax,om,qu,cu,ho,ro)
+type(orientation) function orientation_init(sym,eu,ax,om,qu,cu,ho,ro)
   use prec
   implicit none
-  real(pReal),      intent(in), optional, dimension(3)   :: eu, cu, ho
-  real(pReal),      intent(in), optional, dimension(4)   :: ax, qu, ro
-  real(pReal),      intent(in), optional, dimension(3,3) :: om
- 
+  character(len=pStringLen), intent(in), optional                 :: sym
+  real(pReal),               intent(in), optional, dimension(3)   :: eu, cu, ho
+  real(pReal),               intent(in), optional, dimension(4)   :: ax, qu, ro
+  real(pReal),               intent(in), optional, dimension(3,3) :: om
+
+  if (present(sym)) orientation_init%sym = sym
+
   if (present(om)) then
     call orientation_init%fromRotationMatrix(om)
   endif
