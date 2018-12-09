d9b47f09bc3f5a311194d793bcf4e0eb36c36e9c
Martin Diehl <m.diehl@mpie.de>
2018-12-09 10:52:37 +0100

    modernized
    split arguments logically, assume 3-vector for coordinates

--- processing/post/addAPS34IDEstrainCoords.py
+++ processing/post/addAPS34IDEstrainCoords.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 # -*- coding: UTF-8 no BOM -*-
 
 import os,sys
@@ -19,21 +19,22 @@ Transform X,Y,Z,F APS BeamLine 34 coordinates to x,y,z APS strain coordinates.
 
 """, version = scriptID)
 
-parser.add_option('-f','--frame',       dest='frame', nargs=4, type='string', metavar='string string string string',
-                                        help='APS X,Y,Z coords, and depth F')
-parser.set_defaults(frame = None)
+parser.add_option('-f',
+                  '--frame',
+                  dest='frame',
+                  metavar='string',
+                  help='APS X,Y,Z coords')
+parser.add_option('--depth',
+                  dest='depth',
+                  metavar='string',
+                  help='depth')
 
 (options,filenames) = parser.parse_args()
 
 if options.frame is None:
-  parser.error('no data column specified...')
-
-
-datainfo = {'len':3,
-            'label':[]
-           }
-
-datainfo['label']  += options.frame
+  parser.error('frame not specified')
+if options.depth is None:
+  parser.error('depth not specified')
 
 # --- loop over input files ------------------------------------------------------------------------
 
@@ -49,30 +50,18 @@ for name in filenames:
 
   table.head_read()
 
-  if not table.label_dimension(options.quaternion) == 4:
-    damask.util.croak('input {} does not have dimension 4.'.format(options.quaternion))
-    table.close(dismiss = True)                                                                     # close ASCIItable and remove empty file
+# ------------------------------------------ sanity checks -----------------------------------------
+  errors  = []
+  if table.label_dimension(options.frame) != 3:
+    errors.append('input {} does not have dimension 3.'.format(options.frame))
+  if table.label_dimension(options.depth) != 1:
+    errors.append('input {} does not have dimension 1.'.format(options.depth))
+  if errors  != []:
+    damask.util.croak(errors)
+    table.close(dismiss = True)
     continue
 
-
-
   table.info_append(scriptID + '\t' + ' '.join(sys.argv[1:]))
-
-# --------------- figure out columns to process  ---------------------------------------------------
-  active = []
-  column = {}
-  columnMissing = False
-
-  for label in datainfo['label']:    
-    key = label
-    if key in table.labels(raw = True):
-        active.append(label)
-        column[label] = table.labels.index(key)                                                     # remember columns of requested data
-    else:
-      file['croak'].write('column %s not found...\n'%label)
-      columnMissing = True
-      
-  if columnMissing: continue
 
 # ------------------------------------------ assemble header ---------------------------------------
   table.labels_append(['%i_coord'%(i+1) for i in range(3)])                                         # extend ASCII header with new labels
@@ -83,21 +72,15 @@ for name in filenames:
   RotMat2TSL=np.array([[1.,  0.,            0.],
                        [0.,  np.cos(theta), np.sin(theta)],                                         # Orientation to account for -135 deg
                        [0., -np.sin(theta), np.cos(theta)]])                                        # rotation for TSL convention
-  vec         = np.zeros(4)
-  
   outputAlive = True
   while outputAlive and table.data_read():                                                          # read next data line of ASCII table
-    for i,label in enumerate(active):
-      vec[i] = table.data[column[label]]
+    coord = list(map(float,table.data[table.label_index(options.frame):table.label_index(options.frame)+3]))
+    depth = float(table.data[table.label_index(options.depth)])
 
-    table.data_append(np.dot(RotMat2TSL,np.array([-vec[0], -vec[1],-vec[2]+vec[3]])))
+    table.data_append(np.dot(RotMat2TSL,np.array([-coord[0],-coord[1],-coord[2]+depth])))
 
     outputAlive = table.data_write()                                                                # output processed line
 
-# ------------------------------------------ output result -----------------------------------------
-  outputAlive and table.output_flush()                                                              # just in case of buffered ASCII table
+# ------------------------------------------ output finalization -----------------------------------  
 
-  table.input_close()                                                                               # close input ASCII table (works for stdin)
-  table.output_close()                                                                              # close output ASCII table (works for stdout)
-  if file['name'] != 'STDIN':
-    os.rename(file['name']+'_tmp',file['name'])                                                     # overwrite old one with tmp new
+  table.close()                                                                                     # close ASCII tables
