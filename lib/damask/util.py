4a58fd98f96a6da70e0dec442ccf5e5c15cafda5
Martin Diehl <m.diehl@mpie.de>
2018-12-09 11:02:28 +0100

    pep257 compatible

--- lib/damask/util.py
+++ lib/damask/util.py
@@ -137,6 +137,7 @@ class extendableOption(Option):
 def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
     """
     Call in a loop to create terminal progress bar
+
     @params:
         iteration   - Required  : current iteration (Int)
         total       - Required  : total iterations (Int)
