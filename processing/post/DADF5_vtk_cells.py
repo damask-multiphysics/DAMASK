6b7fd6b7ea7eb4c510be011a011025a76c4676d8
Martin Diehl <mail@martin-diehl.net>
2019-04-18 11:58:17 +0200

    visualizing data from DADF5: first prototype

--- processing/post/DADF5_vtk_cells.py
+++ processing/post/DADF5_vtk_cells.py
@@ -24,22 +24,23 @@ parser.add_argument('filenames', nargs='+',
 
 options = parser.parse_args()
 
+options.labels = ['Fe','Fp','xi_sl']
 
 # --- loop over input files ------------------------------------------------------------------------
 
 for filename in options.filenames:
-  data = damask.DADF5(filename)
+  results = damask.DADF5(filename)
   
-  if data.structured:                                                                               # for grid solvers use rectilinear grid
+  if results.structured:                                                                               # for grid solvers use rectilinear grid
     rGrid = vtk.vtkRectilinearGrid()
     coordArray = [vtk.vtkDoubleArray(),
                   vtk.vtkDoubleArray(),
                   vtk.vtkDoubleArray(),
                  ]
 
-    rGrid.SetDimensions(*(data.grid+1))
+    rGrid.SetDimensions(*(results.grid+1))
     for dim in [0,1,2]:
-      for c in np.linspace(0,data.size[dim],1+data.grid[dim]):
+      for c in np.linspace(0,results.size[dim],1+results.grid[dim]):
         coordArray[dim].InsertNextValue(c)
 
     rGrid.SetXCoordinates(coordArray[0])
@@ -47,22 +48,45 @@ for filename in options.filenames:
     rGrid.SetZCoordinates(coordArray[2])
 
 
-  for i,inc in enumerate(data.increments):
-    data.active['increments'] = [inc]
-    x = data.get_dataset_location('xi_sl')[0]
-    VTKarray = numpy_support.numpy_to_vtk(num_array=data.read_dataset(x,0),deep=True,array_type= vtk.VTK_DOUBLE)
-    VTKarray.SetName('xi_sl')
-    rGrid.GetCellData().AddArray(VTKarray)
-    if data.structured:
+  for i,inc in enumerate(results.increments):
+    print('Output step {}/{}'.format(i+1,len(results.increments)))
+    vtk_data = []
+    results.active['increments'] = [inc]
+    for label in options.labels:
+      for o in results.c_output_types:
+        results.active['c_output_types'] = [o]
+        if o != 'generic':
+          for c in results.constituents:
+            results.active['constituents'] = [c]
+            x = results.get_dataset_location(label)
+            if len(x) == 0:
+              continue
+            array = results.read_dataset(x,0)
+            shape = [array.shape[0],np.product(array.shape[1:])]
+            vtk_data.append(numpy_support.numpy_to_vtk(num_array=array.reshape(shape),deep=True,array_type= vtk.VTK_DOUBLE))
+            vtk_data[-1].SetName('1_'+x[0].split('/',1)[1])
+            rGrid.GetCellData().AddArray(vtk_data[-1])
+        else:
+          results.active['constituents'] = results.constituents
+          x = results.get_dataset_location(label)
+          if len(x) == 0:
+            continue
+          array = results.read_dataset(x,0)
+          shape = [array.shape[0],np.product(array.shape[1:])]
+          vtk_data.append(numpy_support.numpy_to_vtk(num_array=array.reshape(shape),deep=True,array_type= vtk.VTK_DOUBLE))
+          vtk_data[-1].SetName('1_'+x[0].split('/')[1]+'/generic/'+label)
+          rGrid.GetCellData().AddArray(vtk_data[-1])
+          
+    if results.structured:
       writer = vtk.vtkXMLRectilinearGridWriter()
 
     writer.SetCompressorTypeToZLib()
     writer.SetDataModeToBinary()
     writer.SetFileName(os.path.join(os.path.split(filename)[0],
                                     os.path.splitext(os.path.split(filename)[1])[0] +
-                                    '_inc{:04d}'.format(i) +                                        # ToDo: adjust to lenght of increments
+                                    '_inc{:04d}'.format(i) +                                        # ToDo: adjust to length of increments
                                     '.' + writer.GetDefaultFileExtension()))
-    if data.structured:
+    if results.structured:
       writer.SetInputData(rGrid)
     
     writer.Write()
