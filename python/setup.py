a5c620379b7e5c271ffa0158e5285f59bb5be708
Martin Diehl <mail@martin-diehl.net>
2019-04-26 20:54:58 +0200

    tools to build a python wheel distribution

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,20 @@
 import setuptools
+import os
+
+with open(os.path.join(os.path.dirname(__file__),'damask/VERSION')) as f:
+  version = f.readline()[1:-1]
 
 setuptools.setup(
     name="damask",
-    version="0.0.1",
+    version=version,
     author="The DAMASK team",
     author_email="damask@mpie.de",
     description="Python library for DAMASK",
     long_description='test',
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    #long_description_content_type="text/markdown",
+    url="https://damask.mpie.de",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GLPv3",
