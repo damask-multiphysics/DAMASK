1b9355327a49186103d92114bd202a6944ddf73b
Martin Diehl <m.diehl@mpie.de>
2019-04-26 19:08:10 +0200

    WIP: preparing for PIP package

---
+++ python/damask/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="xxx",
+    version="0.0.1",
+    author="The DAMASK team",
+    author_email="damask@mpie.de",
+    description="A small example package",
+    long_description='test',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GLPv3",
+        "Operating System :: OS Independent",
+    ],
+)
