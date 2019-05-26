974f67ffdc70eaa17955115b93610b960c0e6cbd
Martin Diehl <mail@martin-diehl.net>
2019-05-26 12:06:51 +0200

    no frills

--- processing/pre/geom_unpack.py
+++ processing/pre/geom_unpack.py
@@ -1,35 +1,28 @@
 #!/usr/bin/env python3
-# -*- coding: UTF-8 no BOM -*-
 
 import os
 import sys
 from optparse import OptionParser
 from io import StringIO
+
 import damask
+
 
 scriptName = os.path.splitext(os.path.basename(__file__))[0]
 scriptID   = ' '.join([scriptName,damask.version])
+
 
 #--------------------------------------------------------------------------------------------------
 #                                MAIN
 #--------------------------------------------------------------------------------------------------
 
-parser = OptionParser(option_class=damask.extendableOption, usage='%prog options [file[s]]', description = """
-Unpack geometry files containing ranges "a to b" and/or "n of x" multiples (exclusively in one line).
+parser = OptionParser(option_class=damask.extendableOption, usage='%prog [geomfile(s)]', description = """
+Unpack ranges "a to b" and/or "n of x" multiples (exclusively in one line).
 
 """, version = scriptID)
 
-parser.add_option('-1', '--onedimensional',
-                  dest   = 'oneD',
-                  action = 'store_true',
-                  help   = 'output geom file with one-dimensional data arrangement')
-
-parser.set_defaults(oneD = False,
-                   )
-
 (options, filenames) = parser.parse_args()
 
-# --- loop over input files -------------------------------------------------------------------------
 
 if filenames == []: filenames = [None]
 
