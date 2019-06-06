14da4f8e4385c29e96219a3573d3885d7cbdc149
Martin Diehl <m.diehl@mpie.de>
2019-06-06 18:28:10 +0200

    easier to store positions than displacement

--- src/discretization.f90
+++ src/discretization.f90
@@ -11,43 +11,65 @@ module discretization
   private
   
   integer, public, protected :: &
-    discretization_nElem, &
-    discretization_nIP
+    discretization_nIP, &
+    discretization_nElem
+
 
   real(pReal), dimension(:,:), allocatable :: &
-    discretization_Centers_disp, &
-    discretization_Nodes_disp
+    discretization_IPcoords0, &
+    discretization_NodeCoords0, &
+    discretization_IPcoords, &
+    discretization_NodeCoords
 
   public :: &
     discretization_init, &
-    discretization_results
+    discretization_results, &
+    discretization_setIPcoords
 
 contains
   
 
-subroutine discretization_init(nElem,nIP,nNodes)
+subroutine discretization_init(nElem,IPcoords0,NodeCoords0)
 
-  integer, intent(in) :: &
-    nElem, &
-    nIP, &
-    nNodes
+  integer,                     intent(in) :: &
+    nElem
+  real(pReal), dimension(:,:), intent(in) :: &
+    IPcoords0, &
+    NodeCoords0
 
   write(6,'(/,a)')   ' <<<+-  discretization init  -+>>>'
    
-  discretization_nElem = nElem
-  discretization_nIP   = nIP
+  discretization_nElem = nElem 
+  discretization_nIP   = size(IPcoords0,2)
 
-  allocate(discretization_Centers_disp(3,nIP),source    = 0.0_pReal)
-  allocate(discretization_Nodes_disp(  3,nNodes),source = 0.0_pReal)
-
+  discretization_IPcoords0   = IPcoords0
+  discretization_IPcoords    = IPcoords0
+  discretization_NodeCoords0 = NodeCoords0
+  discretization_NodeCoords  = NodeCoords0
+  
 end subroutine discretization_init
 
 
 subroutine discretization_results
 
-  call results_writeDataset('current',discretization_Nodes_disp,'U',  'nodal displacements','m')
-  call results_writeDataset('current',discretization_Centers_disp,'u','IP displacements','m')
+  real(pReal), dimension(:,:), allocatable :: u
+  
+  u =  discretization_NodeCoords -discretization_NodeCoords0
+  call results_writeDataset('current',U,'U','nodal displacements','m')
+  
+  u = discretization_IPcoords -discretization_IPcoords0
+  call results_writeDataset('current',u,'u','IP displacements','m')
 
 end subroutine discretization_results
+
+
+subroutine discretization_setIPcoords(IPcoords)
+
+  real(pReal), dimension(:,:), intent(in) :: IPcoords
+  
+  discretization_IPcoords = IPcoords
+
+end subroutine discretization_setIPcoords
+
 
 end module discretization
--- src/mesh_grid.f90
+++ src/mesh_grid.f90
@@ -197,7 +197,9 @@ subroutine mesh_init(ip,el)
  theMesh%homogenizationAt  = mesh_element(3,:)
  theMesh%microstructureAt  = mesh_element(4,:)
 !!!!!!!!!!!!!!!!!!!!!!!!
-  call discretization_init(product(grid),product(grid),mesh_Nnodes)
+  call discretization_init(product(grid), &
+                           reshape(mesh_ipCoordinates,[3,grid(1)*grid(2)*grid3]),&
+                           mesh_node0)
 
 end subroutine mesh_init
 
