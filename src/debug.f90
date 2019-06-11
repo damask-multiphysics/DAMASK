8956f7f6dace2a9c06e6920cbf5c343f2d6412f5
Martin Diehl <mail@martin-diehl.net>
2019-06-11 14:39:51 +0200

    pInt not needed

--- src/debug.f90
+++ src/debug.f90
@@ -12,49 +12,49 @@ module debug
  implicit none
  private
 
- integer(pInt), parameter, public :: &
-   debug_LEVELSELECTIVE     = 2_pInt**0_pInt, &
-   debug_LEVELBASIC         = 2_pInt**1_pInt, &
-   debug_LEVELEXTENSIVE     = 2_pInt**2_pInt
- integer(pInt), parameter, private :: &
+ integer, parameter, public :: &
+   debug_LEVELSELECTIVE     = 2**0, &
+   debug_LEVELBASIC         = 2**1, &
+   debug_LEVELEXTENSIVE     = 2**2
+ integer, parameter, private :: &
    debug_MAXGENERAL         = debug_LEVELEXTENSIVE                                                  ! must be set to the last bitcode used by (potentially) all debug types
- integer(pInt), parameter, public :: &
-   debug_SPECTRALRESTART    = debug_MAXGENERAL*2_pInt**1_pInt, &
-   debug_SPECTRALFFTW       = debug_MAXGENERAL*2_pInt**2_pInt, &
-   debug_SPECTRALDIVERGENCE = debug_MAXGENERAL*2_pInt**3_pInt, &
-   debug_SPECTRALROTATION   = debug_MAXGENERAL*2_pInt**4_pInt, &
-   debug_SPECTRALPETSC      = debug_MAXGENERAL*2_pInt**5_pInt
+ integer, parameter, public :: &
+   debug_SPECTRALRESTART    = debug_MAXGENERAL*2**1, &
+   debug_SPECTRALFFTW       = debug_MAXGENERAL*2**2, &
+   debug_SPECTRALDIVERGENCE = debug_MAXGENERAL*2**3, &
+   debug_SPECTRALROTATION   = debug_MAXGENERAL*2**4, &
+   debug_SPECTRALPETSC      = debug_MAXGENERAL*2**5
    
- integer(pInt), parameter, public :: &
-   debug_DEBUG                   =  1_pInt, &
-   debug_MATH                    =  2_pInt, &
-   debug_FESOLVING               =  3_pInt, &
-   debug_MESH                    =  4_pInt, &                                                       !< stores debug level for mesh part of DAMASK bitwise coded
-   debug_MATERIAL                =  5_pInt, &                                                       !< stores debug level for material part of DAMASK bitwise coded
-   debug_LATTICE                 =  6_pInt, &                                                       !< stores debug level for lattice part of DAMASK bitwise coded
-   debug_CONSTITUTIVE            =  7_pInt, &                                                       !< stores debug level for constitutive part of DAMASK bitwise coded
-   debug_CRYSTALLITE             =  8_pInt, &
-   debug_HOMOGENIZATION          =  9_pInt, &
-   debug_CPFEM                   = 10_pInt, &
-   debug_SPECTRAL                = 11_pInt, &
-   debug_MARC                    = 12_pInt, &
-   debug_ABAQUS                  = 13_pInt
- integer(pInt), parameter, private :: &
+ integer, parameter, public :: &
+   debug_DEBUG                   =  1, &
+   debug_MATH                    =  2, &
+   debug_FESOLVING               =  3, &
+   debug_MESH                    =  4, &                                                            !< stores debug level for mesh part of DAMASK bitwise coded
+   debug_MATERIAL                =  5, &                                                            !< stores debug level for material part of DAMASK bitwise coded
+   debug_LATTICE                 =  6, &                                                            !< stores debug level for lattice part of DAMASK bitwise coded
+   debug_CONSTITUTIVE            =  7, &                                                            !< stores debug level for constitutive part of DAMASK bitwise coded
+   debug_CRYSTALLITE             =  8, &
+   debug_HOMOGENIZATION          =  9, &
+   debug_CPFEM                   = 10, &
+   debug_SPECTRAL                = 11, &
+   debug_MARC                    = 12, &
+   debug_ABAQUS                  = 13
+ integer, parameter, private :: &
    debug_MAXNTYPE                = debug_ABAQUS                                                     !< must be set to the maximum defined debug type
 
- integer(pInt),protected, dimension(debug_maxNtype+2_pInt),  public :: &                            ! specific ones, and 2 for "all" and "other"
-   debug_level                    = 0_pInt
+ integer,protected, dimension(debug_maxNtype+2),  public :: &                                       ! specific ones, and 2 for "all" and "other"
+   debug_level                    = 0
 
- integer(pInt), protected, public :: &
-   debug_e                       = 1_pInt, &
-   debug_i                       = 1_pInt, &
-   debug_g                       = 1_pInt
+ integer, protected, public :: &
+   debug_e                       = 1, &
+   debug_i                       = 1, &
+   debug_g                       = 1
 
- integer(pInt), dimension(2), public :: &
-   debug_stressMaxLocation       = 0_pInt, &
-   debug_stressMinLocation       = 0_pInt, &
-   debug_jacobianMaxLocation     = 0_pInt, &
-   debug_jacobianMinLocation     = 0_pInt
+ integer, dimension(2), public :: &
+   debug_stressMaxLocation       = 0, &
+   debug_stressMinLocation       = 0, &
+   debug_jacobianMaxLocation     = 0, &
+   debug_jacobianMinLocation     = 0
 
 
  real(pReal), public :: &
@@ -100,17 +100,17 @@ subroutine debug_init
      line = fileContent(j)
      if (IO_isBlank(line)) cycle                                                                    ! skip empty lines
      chunkPos = IO_stringPos(line)
-     tag = IO_lc(IO_stringValue(line,chunkPos,1_pInt))                                              ! extract key
+     tag = IO_lc(IO_stringValue(line,chunkPos,1))                                                   ! extract key
      select case(tag)
        case ('element','e','el')
-         debug_e = IO_intValue(line,chunkPos,2_pInt)
+         debug_e = IO_intValue(line,chunkPos,2)
        case ('integrationpoint','i','ip')
-         debug_i = IO_intValue(line,chunkPos,2_pInt)
+         debug_i = IO_intValue(line,chunkPos,2)
        case ('grain','g','gr')
-         debug_g = IO_intValue(line,chunkPos,2_pInt)
+         debug_g = IO_intValue(line,chunkPos,2)
      end select
 
-     what = 0_pInt
+     what = 0
      select case(tag)
        case ('debug')
          what = debug_DEBUG
@@ -139,12 +139,12 @@ subroutine debug_init
        case ('abaqus')
          what = debug_ABAQUS
        case ('all')
-         what = debug_MAXNTYPE + 1_pInt
+         what = debug_MAXNTYPE + 1
        case ('other')
-         what = debug_MAXNTYPE + 2_pInt
+         what = debug_MAXNTYPE + 2
      end select
      if (what /= 0) then
-       do i = 2_pInt, chunkPos(1)
+       do i = 2, chunkPos(1)
          select case(IO_lc(IO_stringValue(line,chunkPos,i)))
            case('basic')
              debug_level(what) = ior(debug_level(what), debug_LEVELBASIC)
@@ -167,11 +167,11 @@ subroutine debug_init
       endif
    enddo
 
-   do i = 1_pInt, debug_maxNtype
+   do i = 1, debug_maxNtype
      if (debug_level(i) == 0) &
-       debug_level(i) = ior(debug_level(i), debug_level(debug_MAXNTYPE + 2_pInt))                   ! fill undefined debug types with levels specified by "other"
+       debug_level(i) = ior(debug_level(i), debug_level(debug_MAXNTYPE + 2))                        ! fill undefined debug types with levels specified by "other"
 
-     debug_level(i) = ior(debug_level(i), debug_level(debug_MAXNTYPE + 1_pInt))                     ! fill all debug types with levels specified by "all"
+     debug_level(i) = ior(debug_level(i), debug_level(debug_MAXNTYPE + 1))                          ! fill all debug types with levels specified by "all"
    enddo
 
    if (iand(debug_level(debug_debug),debug_LEVELBASIC) /= 0) &
@@ -184,7 +184,7 @@ subroutine debug_init
 !--------------------------------------------------------------------------------------------------
 ! output switched on (debug level for debug must be extensive)
  if (iand(debug_level(debug_debug),debug_LEVELEXTENSIVE) /= 0) then
-     do i = 1_pInt, debug_MAXNTYPE
+     do i = 1, debug_MAXNTYPE
        select case(i)
          case (debug_DEBUG)
            tag = ' Debug'
@@ -241,10 +241,10 @@ end subroutine debug_init
 !--------------------------------------------------------------------------------------------------
 subroutine debug_reset
 
- debug_stressMaxLocation                   = 0_pInt
- debug_stressMinLocation                   = 0_pInt
- debug_jacobianMaxLocation                 = 0_pInt
- debug_jacobianMinLocation                 = 0_pInt
+ debug_stressMaxLocation                   = 0
+ debug_stressMinLocation                   = 0
+ debug_jacobianMaxLocation                 = 0
+ debug_jacobianMinLocation                 = 0
  debug_stressMax                           = -huge(1.0_pReal)
  debug_stressMin                           =  huge(1.0_pReal)
  debug_jacobianMax                         = -huge(1.0_pReal)
@@ -260,8 +260,8 @@ subroutine debug_info
 
  !$OMP CRITICAL (write2out)
    debugOutputCPFEM: if (iand(debug_level(debug_CPFEM),debug_LEVELBASIC) /= 0 &
-                      .and. any(debug_stressMinLocation /= 0_pInt) &
-                      .and. any(debug_stressMaxLocation /= 0_pInt) ) then
+                      .and. any(debug_stressMinLocation /= 0) &
+                      .and. any(debug_stressMaxLocation /= 0) ) then
      write(6,'(2/,a,/)') ' Extreme values of returned stress and Jacobian'
      write(6,'(a39)')                      '                      value     el   ip'
      write(6,'(a14,1x,e12.3,1x,i8,1x,i4)')   ' stress   min :', debug_stressMin, debug_stressMinLocation
