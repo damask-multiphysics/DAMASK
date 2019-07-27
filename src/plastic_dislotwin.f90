f786fcd5f17b93625926ba7f0cb2b032d74e1faf
Vitesh Shah <v.shah@mpie.de>
2019-07-27 11:07:36 +0200

    Better name for climb stress

--- src/plastic_dislotwin.f90
+++ src/plastic_dislotwin.f90
@@ -757,7 +757,7 @@ subroutine plastic_dislotwin_dotState(Mp,T,instance,of)
    v_cl, &                                                                                          !< climb velocity 
    Gamma, &                                                                                         !< stacking fault energy
    tau, &
-   tau_cl, & ! ToDo: MD: good name? It is not a resolved stress but a different projection
+   sigma_cl, & ! ToDo: MD: good name? It is not a resolved stress but a different projection
    b_d
  real(pReal), dimension(param(instance)%sum_N_sl) :: &
    dot_rho_dip_formation, &
@@ -805,7 +805,7 @@ subroutine plastic_dislotwin_dotState(Mp,T,instance,of)
      if (dEq0(rho_dip_distance-rho_dip_distance_min(i))) then
        dot_rho_dip_climb(i) = 0.0_pReal
      else
-       tau_cl = norm2(matmul(Mp,prm%n0_sl(1:3,i))) ! ToDo: MD: correct?
+       sigma_cl = norm2(matmul(Mp,prm%n0_sl(1:3,i))) ! ToDo: MD: correct?
        
        if (prm%SFE_0K > 0.0_pReal) then            ! ToDo: MD: I'm not really sure if this is correct. Maybe Gamma(0K) = 0
          b_d = 24.0_pReal*PI*(1.0_pReal - prm%nu)/(2.0_pReal + prm%nu)* Gamma/(prm%mu*prm%b_sl(i))
@@ -814,7 +814,7 @@ subroutine plastic_dislotwin_dotState(Mp,T,instance,of)
        endif
 
        v_cl = 2.0_pReal*prm%omega*b_d**2.0_pReal*exp(-prm%Qsd/(kB*T)) &
-            * (exp(abs(tau_cl)*prm%b_sl(i)**3.0_pReal/(kB*T)) - 1.0_pReal)
+            * (exp(abs(sigma_cl)*prm%b_sl(i)**3.0_pReal/(kB*T)) - 1.0_pReal)
        dot_rho_dip_climb(i) = 4.0_pReal*v_cl*stt%rho_dip(i,of) &
                             / (rho_dip_distance-rho_dip_distance_min(i))
      endif
