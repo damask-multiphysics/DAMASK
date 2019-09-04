6f07a20919531d510dbd33c91c5be492de3a5928
Martin Diehl <mail@martin-diehl.net>
2019-09-03 17:22:09 -0700

    not sure what this code was intended to do

--- python/damask/solver/solver.py
+++ python/damask/solver/solver.py
@@ -1,18 +1,6 @@
-import damask.solver
-
 class Solver():
   """
   General class for solver specific functionality.
 
   Sub-classed by the individual solvers.
   """
-  
-  def __init__(self,solver=''):
-    solverClass = {
-                      'spectral': damask.solver.Spectral,
-                      'marc':     damask.solver.Marc,
-                    }
-    if solver.lower() in list(solverClass.keys()):
-      self.__class__=solverClass[solver.lower()]
-      self.__init__()
-
