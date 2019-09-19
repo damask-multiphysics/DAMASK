f6c616c84ba813a0d3540b49c4540b68d51e8665
Martin Diehl <mail@martin-diehl.net>
2019-09-19 13:59:18 -0700

    consistent orientation/quaternion handling is in own submodule

--- src/math.f90
+++ src/math.f90
@@ -934,35 +934,6 @@ end function math_Voigt66to3333
 
 
 !--------------------------------------------------------------------------------------------------
-!> @brief quaternion multiplication q1xq2 = q12
-!--------------------------------------------------------------------------------------------------
-pure function math_qMul(A,B)
-
-  real(pReal), dimension(4) ::  math_qMul
-  real(pReal), dimension(4), intent(in) ::  A, B
-
-  math_qMul = [ A(1)*B(1) - A(2)*B(2) - A(3)*B(3) - A(4)*B(4), &
-                A(1)*B(2) + A(2)*B(1) + A(3)*B(4) - A(4)*B(3), &
-                A(1)*B(3) - A(2)*B(4) + A(3)*B(1) + A(4)*B(2), &
-                A(1)*B(4) + A(2)*B(3) - A(3)*B(2) + A(4)*B(1) ]
-
-end function math_qMul
-
-
-!--------------------------------------------------------------------------------------------------
-!> @brief quaternion conjugation
-!--------------------------------------------------------------------------------------------------
-pure function math_qConj(Q)
-
-  real(pReal), dimension(4) ::  math_qConj
-  real(pReal), dimension(4), intent(in) ::  Q
-
-  math_qConj = [Q(1), -Q(2:4)]
-
-end function math_qConj
-
-
-!--------------------------------------------------------------------------------------------------
 !> @brief action of a quaternion on a vector (rotate vector v with Q)
 !--------------------------------------------------------------------------------------------------
 pure function math_qRot(Q,v)
