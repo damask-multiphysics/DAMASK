a26a5738dd24d80ddb3d9bb34d3304494ba94a73
Martin Diehl <mail@martin-diehl.net>
2019-11-21 21:48:54 +0100

    polishing

--- python/tests/test_Rotation.py
+++ python/tests/test_Rotation.py
@@ -8,47 +8,47 @@ class TestRotation:
     def test_Eulers(self):
         for r in range(self.n):
             rot = Rotation.fromRandom()
-            assert  np.allclose(rot.asQuaternion(), 
-                                Rotation.fromEulers(rot.asEulers()).asQuaternion())
+            assert np.allclose(rot.asQuaternion(), 
+                               Rotation.fromEulers(rot.asEulers()).asQuaternion())
 
 
     def test_AxisAngle(self):
         for r in range(self.n):
             rot = Rotation.fromRandom()
-            assert  np.allclose(rot.asEulers(),
-                                Rotation.fromAxisAngle(rot.asAxisAngle()).asEulers())
+            assert np.allclose(rot.asEulers(),
+                               Rotation.fromAxisAngle(rot.asAxisAngle()).asEulers())
 
 
     def test_Matrix(self):
         for r in range(self.n):
             rot = Rotation.fromRandom()
-            assert  np.allclose(rot.asAxisAngle(),
-                                Rotation.fromMatrix(rot.asMatrix()).asAxisAngle())
+            assert np.allclose(rot.asAxisAngle(),
+                               Rotation.fromMatrix(rot.asMatrix()).asAxisAngle())
 
 
     def test_Rodriques(self):
         for r in range(self.n):
             rot = Rotation.fromRandom()
-            assert  np.allclose(rot.asMatrix(),
-                                Rotation.fromRodrigues(rot.asRodrigues()).asMatrix())
+            assert np.allclose(rot.asMatrix(),
+                               Rotation.fromRodrigues(rot.asRodrigues()).asMatrix())
 
 
     def test_Homochoric(self):
         for r in range(self.n):
             rot = Rotation.fromRandom()
-            assert  np.allclose(rot.asRodrigues(),
-                                Rotation.fromHomochoric(rot.asHomochoric()).asRodrigues())
+            assert np.allclose(rot.asRodrigues(),
+                               Rotation.fromHomochoric(rot.asHomochoric()).asRodrigues())
 
 
     def test_Cubochoric(self):
         for r in range(self.n):
             rot = Rotation.fromRandom()
-            assert  np.allclose(rot.asHomochoric(),
-                                Rotation.fromCubochoric(rot.asCubochoric()).asHomochoric())
+            assert np.allclose(rot.asHomochoric(),
+                               Rotation.fromCubochoric(rot.asCubochoric()).asHomochoric())
 
 
     def test_Quaternion(self):
         for r in range(self.n):
             rot = Rotation.fromRandom()
-            assert  np.allclose(rot.asCubochoric(),
-                                Rotation.fromQuaternion(rot.asQuaternion()).asCubochoric())
+            assert np.allclose(rot.asCubochoric(),
+                               Rotation.fromQuaternion(rot.asQuaternion()).asCubochoric())
