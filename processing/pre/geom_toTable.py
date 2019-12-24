9e949556265a80d8c3d041ff85d0d7e0eea0d7b6
Martin Diehl <mail@martin-diehl.net>
2019-12-24 15:58:37 +0100

    wrong coordinates for non-cubical grids

--- processing/pre/geom_toTable.py
+++ processing/pre/geom_toTable.py
@@ -30,7 +30,7 @@ for name in filenames:
     geom = damask.Geom.from_file(StringIO(''.join(sys.stdin.read())) if name is None else name)
     damask.util.croak(geom)
 
-    coord0 = damask.grid_filters.cell_coord0(geom.grid,geom.size,geom.origin).reshape((-1,3),order='F')
+    coord0 = damask.grid_filters.cell_coord0(geom.grid,geom.size,geom.origin).reshape((-1,3))
 
     comments = geom.comments \
              + [scriptID + ' ' + ' '.join(sys.argv[1:]),
