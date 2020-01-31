55e53536f2510887ae9aded05ce74a723f434e66
Martin Diehl <mail@martin-diehl.net>
2020-01-31 21:37:18 +0100

    fixing indentation
    always 2 spaces, not 1 for the first level

--- src/constitutive_plastic_none.f90
+++ src/constitutive_plastic_none.f90
@@ -14,24 +14,24 @@ contains
 !--------------------------------------------------------------------------------------------------
 module subroutine plastic_none_init
 
- integer :: &
-   Ninstance, &
-   p, &
-   NipcMyPhase
-
- write(6,'(/,a)')   ' <<<+-  plastic_'//PLASTICITY_NONE_label//' init  -+>>>'
-
- Ninstance = count(phase_plasticity == PLASTICITY_NONE_ID)
- if (iand(debug_level(debug_constitutive),debug_levelBasic) /= 0) &
-   write(6,'(a16,1x,i5,/)') '# instances:',Ninstance
-
- do p = 1, size(phase_plasticity)
-   if (phase_plasticity(p) /= PLASTICITY_NONE_ID) cycle
-
-   NipcMyPhase = count(material_phaseAt == p) * discretization_nIP
-   call material_allocatePlasticState(p,NipcMyPhase,0,0,0)
-
- enddo
+  integer :: &
+    Ninstance, &
+    p, &
+    NipcMyPhase
+ 
+  write(6,'(/,a)')   ' <<<+-  plastic_'//PLASTICITY_NONE_label//' init  -+>>>'
+ 
+  Ninstance = count(phase_plasticity == PLASTICITY_NONE_ID)
+  if (iand(debug_level(debug_constitutive),debug_levelBasic) /= 0) &
+    write(6,'(a16,1x,i5,/)') '# instances:',Ninstance
+ 
+  do p = 1, size(phase_plasticity)
+    if (phase_plasticity(p) /= PLASTICITY_NONE_ID) cycle
+ 
+    NipcMyPhase = count(material_phaseAt == p) * discretization_nIP
+    call material_allocatePlasticState(p,NipcMyPhase,0,0,0)
+ 
+  enddo
 
 end subroutine plastic_none_init
 
