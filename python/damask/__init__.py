9bb06081875ac3f0df58e7ba4f8ca589584a99cb
Martin Diehl <mail@martin-diehl.net>
2020-03-19 14:40:30 +0100

    maybe this is the cleaner way

--- python/damask/__init__.py
+++ python/damask/__init__.py
@@ -7,16 +7,16 @@ with open(_os.path.join(_os.path.dirname(__file__),'VERSION')) as _f:
     version = _re.sub(r'^v','',_f.readline().strip())
 
 # classes
-from .environment import Environment      # noqa
-from .table       import Table            # noqa
-from .ktv         import VTK              # noqa
-from .colormaps   import Colormap, Color  # noqa
-from .rotation    import Rotation         # noqa
-from .lattice     import Symmetry, Lattice# noqa
-from .orientation import Orientation      # noqa
-from .result      import Result           # noqa
-from .geom        import Geom             # noqa
-from .solver      import Solver           # noqa
+from ._environment import Environment      # noqa
+from .table        import Table            # noqa
+from ._vtk         import VTK              # noqa
+from .colormaps    import Colormap, Color  # noqa
+from .rotation     import Rotation         # noqa
+from .lattice      import Symmetry, Lattice# noqa
+from .orientation  import Orientation      # noqa
+from ._result      import Result           # noqa
+from ._geom        import Geom             # noqa
+from .solver       import Solver           # noqa
 
 # deprecated
 from .asciitable  import ASCIItable       # noqa
