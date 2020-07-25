c55e6edbf375fde92e7f1ff3b8df2d82eadf8a64
Martin Diehl <mail@martin-diehl.net>
2020-07-25 09:59:22 +0200

    removed duplicate

--- src/C_routines.c
+++ src/C_routines.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /* http://stackoverflow.com/questions/30279228/is-there-an-alternative-to-getcwd-in-fortran-2003-2008 */
 
