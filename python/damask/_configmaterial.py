c29e3650f384a4944786562a077e855012a666bc
Martin Diehl <mail@martin-diehl.net>
2020-10-30 22:24:27 +0100

    don't squeeze into scalar

--- python/damask/_configmaterial.py
+++ python/damask/_configmaterial.py
@@ -69,8 +69,8 @@ class ConfigMaterial(Config):
         _,idx = np.unique(np.hstack(list({**constituents_,**kwargs_}.values())),return_index=True,axis=0)
 
         idx = np.sort(idx)
-        constituents_ = {k:v[idx].squeeze() for k,v in constituents_.items()}
-        kwargs_       = {k:v[idx].squeeze() for k,v in kwargs_.items()}
+        constituents_ = {k:np.atleast_1d(v[idx].squeeze()) for k,v in constituents_.items()}
+        kwargs_       = {k:np.atleast_1d(v[idx].squeeze()) for k,v in kwargs_.items()}
 
         return ConfigMaterial().material_add(constituents_,**kwargs_)
 
