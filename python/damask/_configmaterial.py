403ac693dab87d752961bd8732ea77c8f9213204
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2020-12-17 18:08:55 -0500

    need to pass init argument to dict superclass

--- python/damask/_configmaterial.py
+++ python/damask/_configmaterial.py
@@ -9,12 +9,15 @@ from . import Orientation
 class ConfigMaterial(Config):
     """Material configuration."""
 
-    def __init__(self):
-        """Initialize object with all required dictionary keys."""
-        super().__init__()
-        self['material'] = []
-        self['homogenization'] = {}
-        self['phase'] = {}
+    _defaults = {'material': [],
+                 'homogenization': {},
+                 'phase': {}}
+
+    def __init__(self,d={}):
+        """Initialize object with default dictionary keys."""
+        super().__init__(d)
+        for k,v in self._defaults.items():
+            if k not in self: self[k] = v
 
     def save(self,fname='material.yaml',**kwargs):
         """
