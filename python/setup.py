7c6ce525a3bf020bb0956ab2f09f4c2480809230
Martin Diehl <mail@martin-diehl.net>
2021-02-19 06:51:32 +0100

    ask for minimum version

--- python/setup.py
+++ python/setup.py
@@ -6,28 +6,29 @@ with open(Path(__file__).parent/'damask/VERSION') as f:
   version = re.sub(r'(-([^-]*)).*$',r'.\2',re.sub(r'^v(\d+\.\d+(\.\d+)?)',r'\1',f.readline().strip()))
 
 setuptools.setup(
-    name="damask",
+    name='damask',
     version=version,
-    author="The DAMASK team",
-    author_email="damask@mpie.de",
-    description="DAMASK library",
-    long_description="Python library for pre and post processing of DAMASK simulations",
-    url="https://damask.mpie.de",
+    author='The DAMASK team',
+    author_email='damask@mpie.de',
+    description='DAMASK library',
+    long_description='Python library for pre and post processing of DAMASK simulations',
+    url='https://damask.mpie.de',
     packages=setuptools.find_packages(),
     include_package_data=True,
+    python_requires = '>=3.6'
     install_requires = [
-        "pandas",                                                                                   # requires numpy
-        "scipy",
-        "h5py",                                                                                     # requires numpy
-        "vtk",
-        "matplotlib",                                                                               # requires numpy, pillow
-        "pyaml"
+        'pandas>=0.24',                                                                             # requires numpy
+        'scipy>=1.2',
+        'h5py>=2.9',                                                                                # requires numpy
+        'vtk>=8.1',
+        'matplotlib>=3.0',                                                                          # requires numpy, pillow
+        'pyaml>=3.12'
     ],
     classifiers = [
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
     ],
 )
