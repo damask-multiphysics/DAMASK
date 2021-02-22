5517be5078893e79f0146a2ac04bcf0b995d8de7
Martin Diehl <mail@martin-diehl.net>
2021-02-22 08:49:44 +0100

    warn the caller of deprecated functions

--- python/damask/_grid.py
+++ python/damask/_grid.py
@@ -202,7 +202,7 @@ class Grid:
             Geometry file to read.
 
         """
-        warnings.warn('Support for ASCII-based geom format will be removed in DAMASK 3.1.0', DeprecationWarning)
+        warnings.warn('Support for ASCII-based geom format will be removed in DAMASK 3.1.0', DeprecationWarning,2)
         try:
             f = open(fname)
         except TypeError:
@@ -541,7 +541,7 @@ class Grid:
             Compress geometry with 'x of y' and 'a to b'.
 
         """
-        warnings.warn('Support for ASCII-based geom format will be removed in DAMASK 3.1.0', DeprecationWarning)
+        warnings.warn('Support for ASCII-based geom format will be removed in DAMASK 3.1.0', DeprecationWarning,2)
         header =  [f'{len(self.comments)+4} header'] + self.comments \
                 + ['grid   a {} b {} c {}'.format(*self.cells),
                    'size   x {} y {} z {}'.format(*self.size),
