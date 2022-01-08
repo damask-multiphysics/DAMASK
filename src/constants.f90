dd6e9a016efe823fa06c250d6f6f2d59e81226c6
Martin Diehl <mail@martin-diehl.net>
2022-01-08 12:07:29 +0000

    just off by 46 orders of magnitude ;)

--- src/constants.f90
+++ src/constants.f90
@@ -11,6 +11,6 @@ module constants
   real(pReal), parameter :: &
     T_ROOM = 300.0_pReal, &                                                                         !< Room temperature in K. ToDo: IUPAC: 298.15
     K_B = 1.38e-23_pReal, &                                                                         !< Boltzmann constant in J/Kelvin
-    N_A = 6.02214076e-23_pReal                                                                      !< Avogadro constant in 1/mol
+    N_A = 6.02214076e23_pReal                                                                      !< Avogadro constant in 1/mol
 
 end module constants
