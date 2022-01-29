4a84c42112dab832fedd0f354747f7f7aaa5d8dc
Martin Diehl <mail@martin-diehl.net>
2022-01-29 13:49:41 +0100

    improving constants
    IUPAC does not specify an official ambient temperature, T=273.15K from
    standard temperature and pressure (STP) is not particularly suited for our
    purposes

--- src/constants.f90
+++ src/constants.f90
@@ -9,8 +9,8 @@ module constants
   public
 
   real(pReal), parameter :: &
-    T_ROOM = 300.0_pReal, &                                                                         !< Room temperature in K. ToDo: IUPAC: 298.15
-    K_B = 1.38e-23_pReal, &                                                                         !< Boltzmann constant in J/Kelvin
-    N_A = 6.02214076e23_pReal                                                                      !< Avogadro constant in 1/mol
+    T_ROOM = 298.15_pReal, &                                                                        !< Room temperature in K (25°C)/Standard Ambient Temperaure and Pressure (SATP)
+    K_B = 1.380649e-23_pReal, &                                                                     !< Boltzmann constant in J/Kelvin (https://doi.org/10.1351/goldbook)
+    N_A = 6.02214076e23_pReal                                                                       !< Avogadro constant in 1/mol (https://doi.org/10.1351/goldbook)
 
 end module constants
