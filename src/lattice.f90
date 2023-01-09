102d1b79959d9110fea89a5a5e0b731cca4e9802
Martin Diehl <mail@martin-diehl.net>
2023-01-09 23:38:51 +0100

    avoid failing tests
    stiffness matrices should have non-zero diagonal entries, for this
    situation the implemented functionality works as expected

--- src/lattice.f90
+++ src/lattice.f90
@@ -405,13 +405,13 @@ module lattice
 contains
 
 !--------------------------------------------------------------------------------------------------
-!> @brief Module initialization
+!> @brief Run self test.
 !--------------------------------------------------------------------------------------------------
-subroutine lattice_init
+subroutine lattice_init()
 
   print'(/,1x,a)', '<<<+-  lattice init  -+>>>'; flush(IO_STDOUT)
 
-  call selfTest
+  call selfTest()
 
 end subroutine lattice_init
 
@@ -2330,14 +2330,14 @@ subroutine selfTest
           lattice_isotropic_nu(C_tI,'isostress','tI'),1.0e-12_pReal)) error stop 'isotropic_nu/isostress/tI'
 
   call random_number(C)
-  C = lattice_symmetrize_C66(C,'cI')
-  if (dNeq(lattice_isotropic_mu(C,'isostrain','cI'), lattice_isotropic_mu(C,'isostrain','hP'), 5.0e-9_pReal)) &
+  C = lattice_symmetrize_C66(C+math_eye(6),'cI')
+  if (dNeq(lattice_isotropic_mu(C,'isostrain','cI'), lattice_isotropic_mu(C,'isostrain','hP'), 1.0e-12_pReal)) &
     error stop 'isotropic_mu/isostrain/cI-hP'
-  if (dNeq(lattice_isotropic_nu(C,'isostrain','cF'), lattice_isotropic_nu(C,'isostrain','cI'), 5.0e-9_pReal)) &
+  if (dNeq(lattice_isotropic_nu(C,'isostrain','cF'), lattice_isotropic_nu(C,'isostrain','cI'), 1.0e-12_pReal)) &
     error stop 'isotropic_nu/isostrain/cF-tI'
-  if (dNeq(lattice_isotropic_mu(C,'isostress','cI'), lattice_isotropic_mu(C,'isostress'), 5.0e-9_pReal)) &
+  if (dNeq(lattice_isotropic_mu(C,'isostress','cI'), lattice_isotropic_mu(C,'isostress'), 1.0e-12_pReal)) &
     error stop 'isotropic_mu/isostress/cI-hP'
-  if (dNeq(lattice_isotropic_nu(C,'isostress','cF'), lattice_isotropic_nu(C,'isostress'), 5.0e-9_pReal)) &
+  if (dNeq(lattice_isotropic_nu(C,'isostress','cF'), lattice_isotropic_nu(C,'isostress'), 1.0e-12_pReal)) &
     error stop 'isotropic_nu/isostress/cF-tI'
 
 end subroutine selfTest
