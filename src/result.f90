a45a86cfbfb3f7e86c7f13a0ab9fa237cc0e4532
Martin Diehl <mail@martin-diehl.net>
2023-02-04 09:41:18 +0100

    mark DADF5 file layout as stable

--- src/result.f90
+++ src/result.f90
@@ -83,8 +83,8 @@ subroutine result_init(restart)
 
   if (.not. restart) then
     resultFile = HDF5_openFile(getSolverJobName()//'.hdf5','w')
-    call result_addAttribute('DADF5_version_major',0)
-    call result_addAttribute('DADF5_version_minor',14)
+    call result_addAttribute('DADF5_version_major',1)
+    call result_addAttribute('DADF5_version_minor',0)
     call get_command_argument(0,commandLine)
     call result_addAttribute('creator',trim(commandLine)//' '//DAMASKVERSION)
     call result_addAttribute('created',now())
