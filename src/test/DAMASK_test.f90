7dcfb036f9663b89a3d49880833b53eee673283f
Martin Diehl <mail@martin-diehl.net>
2023-02-25 08:39:22 +0100

    more complicated test setup

--- src/test/DAMASK_test.f90
+++ src/test/DAMASK_test.f90
@@ -1,7 +1,15 @@
 program DAMASK_test
-  use prec
+  use parallelization
+  use HDF5_utilities
 
+  use test_prec
+  use test_HDF5_utilities
 
-  call prec_selfTest()
+  call prec_test()
+
+  call parallelization_init()
+  call HDF5_utilities_init()
+
+  call HDF5_utilities_test()
 
 end program DAMASK_test
