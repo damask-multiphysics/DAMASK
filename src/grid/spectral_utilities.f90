6f3b409a25b9a2d1e5d2e3c1bebe1cb313d361a4
Martin Diehl <mail@martin-diehl.net>
2023-07-17 17:48:47 +0200

    enable FFTW planner flags in CAPITALS

--- src/grid/spectral_utilities.f90
+++ src/grid/spectral_utilities.f90
@@ -227,13 +227,13 @@ subroutine spectral_utilities_init()
   end if
 
   select case(IO_lc(num_grid_fft%get_asStr('plan_mode',defaultVal='FFTW_MEASURE')))
-    case('fftw_estimate')                                                                           ! ordered from slow execution (but fast plan creation) to fast execution
+    case('fftw_estimate', 'FFTW_ESTIMATE')                                                          ! ordered from slow execution (but fast plan creation) to fast execution
       FFTW_planner_flag = FFTW_ESTIMATE
-    case('fftw_measure')
+    case('fftw_measure', 'FFTW_MEASURE')
       FFTW_planner_flag = FFTW_MEASURE
-    case('fftw_patient')
+    case('fftw_patient', 'FFTW_PATIENT')
       FFTW_planner_flag = FFTW_PATIENT
-    case('fftw_exhaustive')
+    case('fftw_exhaustive', 'FFTW_EXHAUSTIVE')
       FFTW_planner_flag = FFTW_EXHAUSTIVE
     case default
       call IO_warning(47,'using default FFTW_MEASURE instead of "'//trim(num_grid_fft%get_asStr('plan_mode'))//'"')
