2a88285088385d977345b5e3088ed4314635fa0c
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2023-09-15 10:50:41 -0400

    adapted phenopowerlaw

--- src/crystal.f90
+++ src/crystal.f90
@@ -1395,17 +1395,26 @@ end function crystal_interaction_TwinBySlip
 !--------------------------------------------------------------------------------------------------
 !> @brief Schmid matrix for slip
 !> details only active slip systems are considered
+! Non-schmid projections for cI with up to 6 coefficients
+! https://doi.org/10.1016/j.actamat.2012.03.053, eq. (17)
+! https://doi.org/10.1016/j.actamat.2008.07.037, table 1
 !--------------------------------------------------------------------------------------------------
-function crystal_SchmidMatrix_slip(Nslip,lattice,cOverA) result(SchmidMatrix)
+function crystal_SchmidMatrix_slip(Nslip,lattice,cOverA,nonSchmidCoefficients,sense) result(SchmidMatrix)
 
-  integer,         dimension(:),            intent(in) :: Nslip                                     !< number of active slip systems per family
-  character(len=*),                         intent(in) :: lattice                                   !< Bravais lattice (Pearson symbol)
-  real(pREAL),                              intent(in) :: cOverA
-  real(pREAL),     dimension(3,3,sum(Nslip))           :: SchmidMatrix
+  integer,     dimension(:),              intent(in) :: Nslip                                       !< number of active slip systems per family
+  character(len=*),                       intent(in) :: lattice                                     !< Bravais lattice (Pearson symbol)
+  real(pREAL),                            intent(in) :: cOverA
+  real(pREAL), dimension(:,:), optional,  intent(in) :: nonSchmidCoefficients                       !< non-Schmid coefficients for projections
+  integer,                     optional,  intent(in) :: sense                                       !< sense (-1,+1)
+  real(pREAL), dimension(3,3,sum(Nslip))             :: SchmidMatrix
 
   real(pREAL), dimension(3,3,sum(Nslip))             :: coordinateSystem
   real(pREAL), dimension(:,:),           allocatable :: slipSystems
   integer,     dimension(:),             allocatable :: NslipMax
+  integer,     dimension(:),             allocatable :: slipFamily
+  real(pREAL), dimension(3)                          :: direction, normal, np
+  real(pREAL), dimension(:),             allocatable :: coeff
+  type(tRotation)                                    :: R
   integer                                            :: i
 
   select case(lattice)
@@ -1431,12 +1440,37 @@ function crystal_SchmidMatrix_slip(Nslip,lattice,cOverA) result(SchmidMatrix)
   if (any(Nslip < 0)) &
     call IO_error(144,ext_msg='Nslip '//trim(lattice))
 
+  slipFamily = math_expand([(i, i=1,size(Nslip))],Nslip)
   coordinateSystem = buildCoordinateSystem(Nslip,NslipMax,slipSystems,lattice,cOverA)
+  if (present(sense)) then
+    if (abs(sense) /= 1) error stop 'neither +1 nor -1 sense in crystal_SchmidMatrix_slip'
+    coordinateSystem(1:3,1,1:sum(Nslip)) = coordinateSystem(1:3,1,1:sum(Nslip)) * real(sense,pREAL)
+  end if
 
-  do i = 1, sum(Nslip)
-    SchmidMatrix(1:3,1:3,i) = math_outer(coordinateSystem(1:3,1,i),coordinateSystem(1:3,2,i))
+  do i = 1,sum(Nslip)
+    direction = coordinateSystem(1:3,1,i)
+    normal    = coordinateSystem(1:3,2,i)
+
+    SchmidMatrix(1:3,1:3,i) = math_outer(direction,normal)
     if (abs(math_trace33(SchmidMatrix(1:3,1:3,i))) > tol_math_check) &
       error stop 'dilatational Schmid matrix for slip'
+
+    if (present(nonSchmidCoefficients)) then
+      select case(lattice)
+        case('cI')
+          coeff = nonSchmidCoefficients(slipFamily(i),:)
+          call R%fromAxisAngle([direction,60.0_pREAL],degrees=.true.,P=1)
+          np = R%rotate(normal)
+          SchmidMatrix(1:3,1:3,i) = SchmidMatrix(1:3,1:3,i) &
+                                  + coeff(1) * math_outer(direction, np) &
+                                  + coeff(2) * math_outer(math_cross(normal, direction), normal) &
+                                  + coeff(3) * math_outer(math_cross(np, direction), np) &
+                                  + coeff(4) * math_outer(normal, normal) &
+                                  + coeff(5) * math_outer(math_cross(normal, direction), &
+                                                          math_cross(normal, direction)) &
+                                  + coeff(6) * math_outer(direction, direction)
+      end select
+    end if
   end do
 
 end function crystal_SchmidMatrix_slip
