755fa7e5d8bec3f97c887f67c7d151441a96253c
Franz Roters <f.roters@mpie.de>
2023-10-18 16:21:29 +0200

    make Marc 2023.3 the default version and use it for testing

--- python/damask/solver/_marc.py
+++ python/damask/solver/_marc.py
@@ -4,7 +4,7 @@ import re
 from pathlib import Path
 from typing import Literal
 
-_marc_version = '2023.2'
+_marc_version = '2023.3'
 _marc_root = '/opt/msc'
 _damask_root = str(Path(__file__).parents[3])
 
