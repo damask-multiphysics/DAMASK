correct broadcasting

python/damask/_rotation.py
@@ -1009,11 +1009,14 @@ class Rotation:
 
         """
         a_ = np.array(a,dtype=float)
-        a_ /= np.linalg.norm(a_,axis=-1,keepdims=True)
         b_ = np.array(b,dtype=float)
-        b_ /= np.linalg.norm(b_,axis=-1,keepdims=True)
+
         if a_.shape[-2:] != (2,3) or b_.shape[-2:] != (2,3):
             raise ValueError(f'invalid shape: {a_.shape}/{b_.shape}')
+
+        a_ /= np.linalg.norm(a_,axis=-1,keepdims=True)
+        b_ /= np.linalg.norm(b_,axis=-1,keepdims=True)
+
         am = np.stack([          a_[...,0,:],
                                              a_[...,1,:],
                         np.cross(a_[...,0,:],a_[...,1,:]) ],axis=-1)

python/tests/test_Rotation.py
@@ -789,8 +789,8 @@ class TestRotation:
     def test_parallel(self,multidim_rotations):
         a = np.array([[1.42,0.0,0.0],
                       [0.0,0.3,0.0]])
-        assert Rotation.from_parallel(a,multidim_rotations@a).allclose( multidim_rotations)
-        assert Rotation.from_parallel(multidim_rotations@a,a).allclose(~multidim_rotations)
+        assert Rotation.from_parallel(a,multidim_rotations[...,np.newaxis]@a).allclose( multidim_rotations)
+        assert Rotation.from_parallel(multidim_rotations[...,np.newaxis]@a,a).allclose(~multidim_rotations)
 
 
     @pytest.mark.parametrize('normalize',[True,False])