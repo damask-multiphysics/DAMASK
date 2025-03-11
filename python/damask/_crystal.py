6c133c6c85e995de85a491796e33ea01a0f7c462
Daniel Otto de Mentock <d.mentock@mpie.de>
2025-03-11 18:17:17 +0100

    refine crystal initialization test_add_IPF_color also affected by metadata merge change

--- python/damask/_crystal.py
+++ python/damask/_crystal.py
@@ -721,6 +721,8 @@ class Crystal():
             Angles are given in degrees. Defaults to False.
 
         """
+        if not lattice and not family:
+            raise KeyError(f'Crystal initialization requires either lattice or family information')
         if family is not None and family not in list(lattice_symmetries.values()):
             raise KeyError(f'invalid crystal family "{family}"')
         if lattice is not None and family is not None and family != lattice_symmetries[lattice]:
