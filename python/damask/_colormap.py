bugfix: correct calculation of hue diff
relevant for the case of hues with a large difference.
Also fixing some glitches in docstrings

python/damask/_colormap.py
@@ -78,7 +78,7 @@ class Colormap(mpl.colors.ListedColormap):
         Returns
         -------
         eq : bool
-            Wheter self equals other.
+            Whether self equals other.
         """
         if not isinstance(other, Colormap):
             return NotImplemented
@@ -523,15 +523,15 @@ class Colormap(mpl.colors.ListedColormap):
         | https://www.kennethmoreland.com/color-maps/ColorMapsExpanded.pdf
         | https://www.kennethmoreland.com/color-maps/diverging_map.py
         """
-        def rad_diff(a,b):
+        def rad_diff(h_1,h_2):
             """
             Compute angular difference between two hue orientations.
 
             Parameters
             ----------
-            a : float
+            h_1 : float
                 First hue orientation.
-            b : float
+            h_2 : float
                 Second hue orientation.
 
             Returns
@@ -539,7 +539,8 @@ class Colormap(mpl.colors.ListedColormap):
             d : float
                 Angular difference between given hue orientations.
             """
-            return abs(a[2]-b[2])
+            d = abs(h_1-h_2)%(2*np.pi)
+            return min(d,2*np.pi-d)
 
         def adjust_hue(msh_sat, m_unsat):
             """
@@ -567,7 +568,7 @@ class Colormap(mpl.colors.ListedColormap):
         lo = np.array(low)
         hi = np.array(high)
 
-        if (lo[1] > 0.05 and hi[1] > 0.05 and rad_diff(lo,hi) > np.pi/3.0):
+        if (lo[1] > 0.05 and hi[1] > 0.05 and rad_diff(lo[2],hi[2]) > np.pi/3.0):
             M_mid = max(lo[0],hi[0],88.0)
             if frac < 0.5:
                 hi = np.array([M_mid,0.0,0.0])

python/damask/_crystal.py
@@ -819,7 +819,7 @@ class Crystal():
         Returns
         -------
         equal : bool
-            Wheter both arguments are equal.
+            Whether both arguments are equal.
         """
         return (NotImplemented if not isinstance(other, Crystal) else
                 self.lattice == other.lattice and
@@ -896,7 +896,7 @@ class Crystal():
         Returns
         -------
         labels : list of str
-           Short labels of the orientation relationships.
+            Labels of the applicable orientation relationships.
         """
         return [k for k,v in orientation_relationships.items() if np.any([m.startswith(str(self.lattice)) for m in v])]
 
@@ -1138,8 +1138,8 @@ class Crystal():
 
         Returns
         -------
-        basis_real : numpy.ndarray, shape(3)
-            Reciprocal (dual) rystal basis.
+        basis_reciprocal : numpy.ndarray, shape(3)
+            Reciprocal (dual) crystal basis.
         """
         return np.linalg.inv(self.basis_real.T)
 

python/damask/_rotation.py
@@ -153,7 +153,7 @@ class Rotation:
         Returns
         -------
         equal : numpy.ndarray
-            Wheter both arguments are equal.
+            Whether both arguments are equal.
         """
         return NotImplemented if not isinstance(other, Rotation) else \
                np.logical_or(np.all(self.quaternion ==     other.quaternion,axis=-1),
@@ -2484,7 +2484,7 @@ class Rotation:
            Coordinates of a point on a uniform refinable grid on a ball or
            in a uniform refinable cubical grid.
         direction : {'forward', 'backward'}
-            Wheter to map from ball to cube or from cube to ball.
+            Whether to map from ball to cube or from cube to ball.
 
         References
         ----------