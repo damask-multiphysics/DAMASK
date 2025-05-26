bugfix: correct calculation of hue diff
backport of fc98f313a6a1fb4a127a8577cf1befc75edba5a4

python/damask/_colormap.py
@@ -465,8 +465,9 @@ class Colormap(mpl.colors.ListedColormap):
         | https://www.kennethmoreland.com/color-maps/diverging_map.py
 
         """
-        def rad_diff(a,b):
-            return abs(a[2]-b[2])
+        def rad_diff(h_1,h_2):
+            d = abs(h_1-h_2)%(2*np.pi)
+            return min(d,2*np.pi-d)
 
         def adjust_hue(msh_sat, msh_unsat):
             """If saturation of one of the two colors is much less than the other, hue of the less."""
@@ -480,7 +481,7 @@ class Colormap(mpl.colors.ListedColormap):
         lo = np.array(low)
         hi = np.array(high)
 
-        if (lo[1] > 0.05 and hi[1] > 0.05 and rad_diff(lo,hi) > np.pi/3.0):
+        if (lo[1] > 0.05 and hi[1] > 0.05 and rad_diff(lo[2],hi[2]) > np.pi/3.0):
             M_mid = max(lo[0],hi[0],88.0)
             if frac < 0.5:
                 hi = np.array([M_mid,0.0,0.0])