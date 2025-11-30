73e5bab30f060f0022b0af5e17aaa3d84d477fc3
Martin Diehl <mail@martin-diehl.net>
2025-11-30 07:22:29 +0100

    provide symlink to executable in lower letters
    damask_grid and damask_mesh are more in alignment with common
    conventions. Full transition should happen with DAMASK 4.0

--- env/DAMASK.sh
+++ env/DAMASK.sh
@@ -89,8 +89,8 @@ fi
 
 export DAMASK_ROOT
 export PYTHONPATH=$DAMASK_ROOT/python:$PYTHONPATH
-source "$ENV_ROOT/_damask_grid.sh"
-source "$ENV_ROOT/_damask_mesh.sh"
+source "$ENV_ROOT/damask_grid"
+source "$ENV_ROOT/damask_mesh"
 
 for var in BASE STAT SOLVER BRANCH ENV_ROOT; do
   unset "${var}"
