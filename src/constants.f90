329299131a572b8b12d86342f460274569f21192
Martin Diehl <mail@martin-diehl.net>
2026-09-03 06:23:20 +0200

    consistent style/naming

--- src/constants.f90
+++ src/constants.f90
@@ -11,9 +11,9 @@ module constants
 
   real(pREAL), parameter :: &
     T_ROOM = 293.15_pREAL, &                                                                        !< Room temperature (20°C) in K (https://en.wikipedia.org/wiki/ISO_1)
-    K_B = 1.380649e-23_pREAL, &                                                                     !< Boltzmann constant in J/Kelvin (https://doi.org/10.1351/goldbook)
-    N_A = 6.02214076e23_pREAL, &                                                                    !< Avogadro constant in 1/mol (https://doi.org/10.1351/goldbook)
-    R = 8.314462618_pREAL                                                                           !< gas constant: R = N_A * K_B, https://en.wikipedia.org/wiki/Gas_constant
+    K_B = 1.380649e-23_pREAL, &                                                                     !< Boltzmann constant in J/K (https://doi.org/10.1103/RevModPhys.97.025002)
+    N_A = 6.02214076e23_pREAL, &                                                                    !< Avogadro constant in 1/mol (https://doi.org/10.1103/RevModPhys.97.025002)
+    R = K_B * N_A                                                                                   !< gas constant in J/(K mol)
 
   real(pREAL), dimension(2), parameter :: &
     WGT_MANDEL = [sqrt(2.0_pREAL), 2.0_pREAL]                                                       !< Weights for Mandel notation
